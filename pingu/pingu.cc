a6bdc8d48edf1e1246e204ea6292d06f334f43d6
Tom7 <499244+tom7@users.noreply.github.com>
2022-01-03 04:05:37 +0000

    slow asynchronous operation
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4491 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- pingu/pingu.cc
+++ pingu/pingu.cc
@@ -23,15 +23,19 @@
 // This is from config.h but I want to avoid autoconf.
 #define PACKAGE_VERSION "1.29.11"
 
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
+
 static constexpr int BLOCK_SIZE = 512;
 struct Block {
-  Block() {
+  Block(int id) : id(id) {
 	bzero(contents.data(), BLOCK_SIZE);
   }
 
   // Read the indicated portion of the block into the buffer.
   void Read(uint8_t *buf, int start, int count) {
+	nbdkit_debug("try read from block %d", id);
 	MutexLock ml(&mutex);
+	nbdkit_debug("ready to read from block %d", id);
 	for (int i = 0; i < count; i++) {
 	  *buf = contents[start + i];
 	  buf++;
@@ -47,6 +51,7 @@ struct Block {
 	}
   }
 
+  const int id = 0;
   std::mutex mutex;
   std::array<uint8_t, BLOCK_SIZE> contents;
 };
@@ -56,7 +61,7 @@ struct Block {
 struct Blocks {
   explicit Blocks(int64_t num_blocks) : num_blocks(num_blocks) {
 	for (int i = 0; i < num_blocks; i++)
-	  mem.push_back(new Block);
+	  mem.push_back(new Block(i));
   }
 
   inline void Read(int block_idx, uint8_t *buf, int start, int count) {
@@ -78,16 +83,80 @@ struct Blocks {
   std::vector<Block *> mem;
 };
 
+// keeps all the locks locked except for one
+struct SlowDowner {
+  SlowDowner(Blocks *blocks) : blocks(blocks) {
+	for (int i = 0; i < blocks->num_blocks; i++) {
+	  blocks->mem[i]->mutex.lock();
+	}
+
+    unlock_thread.reset(new std::thread(&SlowDowner::UnlockThread, this));
+	nbdkit_debug("thread spawned %p\n", unlock_thread.get());
+  }
+
+  void UnlockThread() {
+	printf("in the thread");
+	nbdkit_debug("unlock thread started\n");
+	for (;;) {
+	  for (int i = 0; i < blocks->num_blocks; i++) {
+		blocks->mem[i]->mutex.unlock();
+		nbdkit_debug("%d is unlocked", i);
+		std::this_thread::sleep_for(
+			std::chrono::milliseconds(50));
+		blocks->mem[i]->mutex.lock();
+
+		{
+		  MutexLock ml(&mutex);
+		  if (should_die)
+			goto die;
+		}
+	  }
+	}
+
+  die:
+	// unlock everything
+	for (int j = 0; j < blocks->num_blocks; j++) {
+	  blocks->mem[j]->mutex.unlock();
+	}
+  }
+
+  ~SlowDowner() {
+	{
+	  MutexLock ml(&mutex);
+	  should_die = true;
+	}
+	unlock_thread->join();
+	unlock_thread.reset(nullptr);
+  }
+
+  std::mutex mutex;
+  bool should_die = false;
+  std::unique_ptr<std::thread> unlock_thread;
+  Blocks *blocks = nullptr;
+};
+
 static int64_t num_blocks = -1;
 // Allocated buffer.
 static Blocks *blocks = nullptr;
 
-static void example_unload(void) {
+static SlowDowner *slowdowner = nullptr;
+
+static int pingu_after_fork(void) {
+  nbdkit_debug("After fork! num_blocks %lu\n", num_blocks);
+  blocks = new Blocks(num_blocks);
+  slowdowner = new SlowDowner(blocks);
+  return 0;
+}
+
+static void pingu_unload(void) {
+  delete slowdowner;
+  slowdowner = nullptr;
+
   delete blocks;
   blocks = nullptr;
 }
 
-static int example_config(const char *key, const char *value) {
+static int pingu_config(const char *key, const char *value) {
   if (strcmp(key, "num_blocks") == 0) {
     num_blocks = nbdkit_parse_size(value);
     if (num_blocks == -1)
@@ -100,7 +169,7 @@ static int example_config(const char *key, const char *value) {
   return 0;
 }
 
-static int example_config_complete(void) {
+static int pingu_config_complete(void) {
   if (num_blocks == -1) {
     nbdkit_error("you must specify num_blocks=<NUM> on the command line");
     return -1;
@@ -108,58 +177,49 @@ static int example_config_complete(void) {
   return 0;
 }
 
-#define example_config_help \
+#define pingu_config_help \
   "num_blocks=<NUM>  (required) Number of blocks in the backing buffer"
 
-static void example_dump_plugin(void) {
+static void pingu_dump_plugin(void) {
 }
 
-static int example_get_ready(void) {
-  nbdkit_debug("Get Ready! Num_Blocks %lu\n", num_blocks);
-  blocks = new Blocks(num_blocks);
+[[maybe_unused]]
+static int pingu_get_ready(void) {
+  // ?
   return 0;
 }
 
 /* Create the per-connection handle. */
-static void *example_open(int readonly) {
+static void *pingu_open(int readonly) {
   return NBDKIT_HANDLE_NOT_NEEDED;
 }
 
-#define THREAD_MODEL NBDKIT_THREAD_MODEL_PARALLEL
-
 /* Get the disk size. */
-static int64_t example_get_size(void *handle) {
+static int64_t pingu_get_size(void *handle) {
   return num_blocks * BLOCK_SIZE;
 }
 
 /* Flush is a no-op, so advertise native FUA support */
-static int example_can_fua(void *handle) {
+static int pingu_can_fua(void *handle) {
   return NBDKIT_FUA_NATIVE;
 }
 
 /* Serves the same data over multiple connections. */
-static int example_can_multi_conn(void *handle) {
+static int pingu_can_multi_conn(void *handle) {
   return 1;
 }
 
 /* Cache. */
-static int example_can_cache(void *handle) {
+static int pingu_can_cache(void *handle) {
   /* Everything is already in memory, returning this without
    * implementing .cache lets nbdkit do the correct no-op.
    */
   return NBDKIT_CACHE_NATIVE;
 }
 
-#if 0
-/* Fast zero. */
-static int example_can_fast_zero(void *handle) {
-  return 1;
-}
-#endif
-
 /* Read data. */
 // read count bytes starting at offset into buf
-static int example_pread(void *handle, void *buf_void,
+static int pingu_pread(void *handle, void *buf_void,
                          uint32_t count, uint64_t offset,
                          uint32_t flags) {
   uint8_t *buf = (uint8_t*)buf_void;
@@ -192,7 +252,7 @@ static int example_pread(void *handle, void *buf_void,
 }
 
 /* Write data. */
-static int example_pwrite(void *handle, const void *buf_void,
+static int pingu_pwrite(void *handle, const void *buf_void,
                           uint32_t count, uint64_t offset,
                           uint32_t flags) {
   const uint8_t *buf = (const uint8_t*)buf_void;
@@ -218,62 +278,24 @@ static int example_pwrite(void *handle, const void *buf_void,
   }
 }
 
-#if 0
-/* Zero. */
-static int example_zero(void *handle,
-                        uint32_t count, uint64_t offset,
-                        uint32_t flags) {
-  /* Flushing, and thus FUA flag, is a no-op. Assume that
-   * a->f->zero generally beats writes, so FAST_ZERO is a no-op. */
-  assert((flags & ~(NBDKIT_FLAG_FUA | NBDKIT_FLAG_MAY_TRIM |
-                    NBDKIT_FLAG_FAST_ZERO)) == 0);
-  bzero(a + offset, count);
-  return 0;
-}
-
-/* Trim (same as zero). */
-static int example_trim(void *handle,
-                        uint32_t count, uint64_t offset,
-                        uint32_t flags) {
-  /* Flushing, and thus FUA flag, is a no-op */
-  assert((flags & ~NBDKIT_FLAG_FUA) == 0);
-  bzero(a + offset, count);
-  return 0;
-}
-#endif
-
 /* Nothing is persistent, so flush is trivially supported */
-static int example_flush(void *handle, uint32_t flags) {
+static int pingu_flush(void *handle, uint32_t flags) {
   return 0;
 }
-
-#if 0
-/* Extents. */
-static int example_extents(void *handle, uint32_t count, uint64_t offset,
-                           uint32_t flags, struct nbdkit_extents *extents) {
-  // Probably just for debugging? Always fails.
-  return -1;
-
-  // maybe can do it with this?
-  /*
-    if (nbdkit_add_extent(extents, offset, n, type) == -1)
-    return -1;
-  */
-}
-#endif
 
 // Note: In C++, these fields have to be in the same order in which
 // they are declared in nbdkit-plugin.h.
 static struct nbdkit_plugin plugin = {
-  .name              = "example",
+  .name              = "pingu",
   .version           = PACKAGE_VERSION,
-  .unload            = example_unload,
-  .config            = example_config,
-  .config_complete   = example_config_complete,
-  .config_help       = example_config_help,
+  .load              = nullptr,
+  .unload            = pingu_unload,
+  .config            = pingu_config,
+  .config_complete   = pingu_config_complete,
+  .config_help       = pingu_config_help,
 
-  .open              = example_open,
-  .get_size          = example_get_size,
+  .open              = pingu_open,
+  .get_size          = pingu_get_size,
 
   .can_write = nullptr,
   .can_flush = nullptr,
@@ -284,34 +306,34 @@ static struct nbdkit_plugin plugin = {
    * paths from failed system calls.
    */
   .errno_is_preserved = 1,
-  .dump_plugin       = example_dump_plugin,
+  .dump_plugin       = pingu_dump_plugin,
 
   .can_zero = nullptr,
-  .can_fua           = example_can_fua,
+  .can_fua           = pingu_can_fua,
 
-  .pread             = example_pread,
-  .pwrite            = example_pwrite,
-  .flush             = example_flush,
+  .pread             = pingu_pread,
+  .pwrite            = pingu_pwrite,
+  .flush             = pingu_flush,
   .trim              = nullptr,
   .zero              = nullptr,
 
   .magic_config_key  = "num_blocks",
-  .can_multi_conn    = example_can_multi_conn,
+  .can_multi_conn    = pingu_can_multi_conn,
 
   .can_extents       = nullptr,
   .extents           = nullptr,
 
-  .can_cache         = example_can_cache,
+  .can_cache         = pingu_can_cache,
   .cache             = nullptr,
 
   .thread_model      = nullptr,
 
-  .can_fast_zero     = nullptr, // example_can_fast_zero,
+  .can_fast_zero     = nullptr, // pingu_can_fast_zero,
 
   .preconnect        = nullptr,
 
-  .get_ready         = example_get_ready,
-  .after_fork = nullptr,
+  .get_ready         = nullptr,
+  .after_fork        = pingu_after_fork,
 
 };
 
---
+++ pingu/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ $(id -u) -ne 0 ]] ;
+then echo "Please run as root" ;
+	 exit -1 ;
+fi
+
+set +x
+
+SOCKET=/tmp/nbdsocket.pingu
+
+# make sure this is loaded (e.g. after reboot)
+modprobe nbd
+
+# clean up any existing
+umount /mnt/pingu
+# XXX force same block size as server
+# hour-long timeout
+nbd-client -timeout 3600 -d /dev/nbd0
+killall -9 nbdkit
+rm -f "$SOCKET"
+
+if [ "$1" = "stop" ]; then
+	echo "Stopped."
+	df -k /mnt/pingu
+	exit 0
+fi
+
+# Note: 65536 bytes (128 blocks) is too small for FAT
+
+# note that the argument to --run is single-quoted; we do not
+# want bash to try to do the interpolation. $unixsocket is an
+# nbdkit concept
+
+
+../../nbdkit/server/nbdkit --verbose -U "$SOCKET" ./pingu.so 256 --run './mount.sh $unixsocket'
+# drop-in replacement with memory plugin:
+# ../../nbdkit/server/nbdkit --verbose -U "$SOCKET" ../../nbdkit/plugins/memory/.libs/nbdkit-memory-plugin.so 131072 --run './mount.sh $unixsocket'
+
