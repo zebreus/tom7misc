da271dc9a87af4d7d1549ea4acc35d6ca3b487fe
Tom7 <499244+tom7@users.noreply.github.com>
2022-01-03 04:24:59 +0000

    slow asynchronous operation
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4492 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

---
+++ pingu/mount.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [[ $(id -u) -ne 0 ]] ;
+then echo "Please run as root" ;
+	 exit -1 ;
+fi
+
+echo "$1"
+
+if [[ "$1" = "" ]] ;
+then echo "Needs the unix socket; call from run.sh" ;
+	 exit -1 ;
+fi
+
+echo "STARTED SERVER on $1"
+
+# nbd-client localhost for TCP
+nbd-client -unix "$1" /dev/nbd0 || exit -1
+
+gdisk /dev/nbd0 <<EOF
+n
+1
+
+
+8300
+w
+y
+
+EOF
+
+# -a disables alignment, saving space
+
+mkfs.fat -v -a -n "PINGU" /dev/nbd0p1 || exit -1
+mount /dev/nbd0p1 /mnt/pingu || exit -1
+
+df -k /mnt/pingu
+echo "OK"
+
+# enter a prompt
+bash
+
+
+# when the prompt exits, try to clean up
+nbd-client -d /dev/nbd0
