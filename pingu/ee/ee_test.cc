0a07cf53c2bb96e7073b63ae9b0358bec08526ce
Tom7 <499244+tom7@users.noreply.github.com>
2022-01-25 02:46:44 +0000

    Cool, this can read EEPROM now... it was just a soldering problem
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4516 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- pingu/ee/ee_test.cc
+++ pingu/ee/ee_test.cc
@@ -11,28 +11,23 @@
 using uint8 = uint8_t;
 using uint32 = uint32_t;
 
+static string CodeString(int code) {
+  switch (code) {
+  case BCM2835_I2C_REASON_OK: return "OK";
+  case BCM2835_I2C_REASON_ERROR_NACK: return "NACK";
+  case BCM2835_I2C_REASON_ERROR_CLKT: return "Clock stretch";
+  case BCM2835_I2C_REASON_ERROR_DATA:
+    return "not all data sent / timeout";
+  default: return "other??";
+  }
+}
+
 [[maybe_unused]]
 static bool WriteVec(const std::vector<uint8> &msg) {
-
   uint8_t code =
     bcm2835_i2c_write((const char *)&msg[0], msg.size());
-  switch (code) {
-  case BCM2835_I2C_REASON_OK:
-    printf("I2C OK\n");
-    return true;
-  case BCM2835_I2C_REASON_ERROR_NACK:
-    // printf("I2C NACK\n");
-    return false;
-  case BCM2835_I2C_REASON_ERROR_CLKT:
-    printf("I2C Clock stretch\n");
-    return false;
-  case BCM2835_I2C_REASON_ERROR_DATA:
-    printf("i2c not all data sent / timeout\n");
-    return false;
-  default:
-    printf("i2c other error??\n");
-    return false;
-  }
+  printf("i2c %s\n", CodeString(code).c_str());
+  return code == BCM2835_I2C_REASON_OK;
 }
 
 [[maybe_unused]]
@@ -50,15 +45,6 @@ static void Enumerate() {
   // supposedly this supports 100khz and 400khz.
   bcm2835_i2c_set_baudrate(100000);
 
-  // for (int a = 0; a < 256; a++) {
-  for (uint8_t a : {0b10100001}) {
-  // printf("Try address 0x%02x\n", a);
-    // Test..
-    // constexpr uint8_t a = 0b10100000;
-  printf("Try address 0x%02x\n", a);
-  bcm2835_i2c_setSlaveAddress(a);
-
-
   //   7     6     5     4     3     2     1     0
   //  device type ident.   | chip sel  | addr8 | r/w
   //   1     0     1     0     0     0     A     R
@@ -66,38 +52,40 @@ static void Enumerate() {
   // chip is wired to 00.
   //  R=read
   //  A=high bit of address
-  // const uint8 cmd = 0b10100001;
+  // The R/W bit is implemented by the i2c peripheral
+  // (using 7-bit addressing). So we have two:
+  [[maybe_unused]]
+  constexpr uint8_t addr0 = 0b01010000;
+  [[maybe_unused]]
+  constexpr uint8_t addr1 = 0b01010001;
 
-  for (int i = 0; i < 3; i++) {
-    char buf[8] = {};
-    // int code = bcm2835_i2c_read(buf, 1);
-    char reg[1] = {0x01};
-    int code = bcm2835_i2c_read_register_rs(reg, buf, 1);
+  bcm2835_i2c_setSlaveAddress(addr0);
 
-    switch (code) {
-    case BCM2835_I2C_REASON_OK:
-      printf("I2C OK\n"
-             "read! address 0x%02x got %02x", a, buf[0]);
-      return;
-    case BCM2835_I2C_REASON_ERROR_NACK:
-      printf("I2C NACK\n");
-      break;
-    case BCM2835_I2C_REASON_ERROR_CLKT:
-      printf("I2C Clock stretch\n");
-      break;
-    case BCM2835_I2C_REASON_ERROR_DATA:
-      printf("i2c not all data sent / timeout\n");
-      break;
-    default:
-      printf("i2c other error??\n");
-      break;
-    }
+  constexpr int SIZE = 512;
+  char buf[SIZE];
+  [[maybe_unused]] char reg = 0x04;
+  // load address to read from by doing a dummy write
+  WriteVec({0});
+  // then read 16 bytes (or whatever)
+  int code = bcm2835_i2c_read(buf, SIZE);
+  printf("i2c read: %s\n", CodeString(code).c_str());
 
-      usleep(1000);
+  printf("Result:");
+  for (int i = 0; i < SIZE; i++) {
+    if (i % 16 == 0) printf("\n");
+    char c = buf[i];
+    printf(" %02x", c);
+    #if 0
+      if (c >= 32 && c < 128)
+        printf(" %c", c);
+      else
+        printf(" _");
+    #endif
   }
-  }
+  printf("\n");
 
   bcm2835_i2c_end();
+  return;
 }
 
 int main(int argc, char **argv) {
