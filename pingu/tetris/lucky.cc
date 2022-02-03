622d9b6dcc2843bacb5c5d7aaf22527a47324cfb
Tom7 <499244+tom7@users.noreply.github.com>
2022-02-03 06:00:23 +0000

    Plenty of RNG analysis and visualization. Demonstrate that alternating pieces is pretty doable
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4523 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- pingu/tetris/lucky.cc
+++ pingu/tetris/lucky.cc
@@ -4,6 +4,7 @@
 #include <memory>
 #include <cstdint>
 #include <cmath>
+#include <unordered_set>
 
 #include "../fceulib/emulator.h"
 #include "../fceulib/simplefm2.h"
@@ -76,6 +77,8 @@ struct State {
 // This is the code at 0xAB47. Every JSR to this sets
 // X = 17 (M_RNG1) and Y = 2 (its size), so we just
 // take those as constants.
+//
+// I think this is a two-tap LFSR. Its period is 32767.
 [[maybe_unused]]
 static State NextRNG(State s) {
   uint8 x = 17;
@@ -207,9 +210,14 @@ static State NextPiece(State s) {
   a += s.drop_count;
 
  _992A:
-  // XXX check that this is unsigned, but
-  // it would be weird if not, since then we would
-  // read outside the table below
+  // This code is (a %= 7) implemented as a loop,
+  // which is not really necessary since a is
+  // known to be in [0,7] due to the AND above.
+  // We could simply check for == 7 and replace
+  // with zero in that case, or just use the fact
+  // that the table at position 7 contains the
+  // same entry as at zero!
+
   //  cmp #$07
   carry_flag = (a >= 0x07);
   //  bcc $9934
@@ -472,7 +480,7 @@ static void PieceDrop() {
 
 // It doesn't work... we get stuck!
 [[maybe_unused]]
-static void MakeStreak() {
+static void MakeStreak1() {
 
   ArcFour rc("tetris");
 
@@ -612,6 +620,209 @@ static void MakeStreak() {
 }
 
 [[maybe_unused]]
+static void MakeStreak2() {
+
+  // 0-based column.
+  uint8 oriented_l = 0x0D;
+  int offset_l = 0;
+  // 1-based
+  uint8 oriented_7 = 0x0F;
+  int offset_7 = 1;
+  // 1-based
+  uint8 oriented_j = 0x04;
+  int offset_j = 1;
+  // 0-based
+  uint8 oriented_f = 0x06;
+  int offset_f = 0;
+
+  uint8 oriented_o = 0x0A;
+  int offset_o = 1;
+
+  std::vector<std::tuple<Piece, uint8, int>> schedule = {
+    // piece type, desired orientation, column
+    make_tuple(PIECE_O, oriented_o, 0 + offset_o),
+    make_tuple(PIECE_L, oriented_l, 2 + offset_l),
+    make_tuple(PIECE_O, oriented_o, 0 + offset_o),
+    make_tuple(PIECE_J, oriented_j, 4 + offset_j),
+    make_tuple(PIECE_L, oriented_7, 2 + offset_7),
+    make_tuple(PIECE_J, oriented_f, 4 + offset_f),
+    make_tuple(PIECE_L, oriented_l, 6 + offset_l),
+    make_tuple(PIECE_J, oriented_j, 8 + offset_j),
+    make_tuple(PIECE_L, oriented_7, 6 + offset_7),
+    make_tuple(PIECE_J, oriented_f, 8 + offset_f),
+  };
+
+  for (const auto &[piece, orientation, offset_] : schedule) {
+    CHECK(piece == DecodePiece(orientation));
+  }
+
+  ArcFour rc("tetris");
+
+  std::unique_ptr<Emulator> emu;
+  emu.reset(Emulator::Create("tetris.nes"));
+  CHECK(emu.get() != nullptr);
+
+  vector<uint8> startmovie =
+    SimpleFM7::ParseString(
+        "!" // 1 player
+        "554_7t" // press start on main menu
+        "142_"
+        "7c" // signals a place where we can wait to affect RNG (maybe)
+        "45_5t" // press start on game type menu
+        "82_7c" // another place to potentially wait (likely?)
+        // "81_" // computed wait
+        "13_" // computed wait for L|O, to match schedule below
+        "8_7t" // press start on A-type menu to begin game
+        "68_" // game started by now
+                           );
+
+  for (uint8 c : startmovie) emu->Step(c, 0);
+  SaveScreenshot("start.png", emu.get());
+
+  vector<uint8> outmovie = startmovie;
+
+  vector<uint8> savestate;
+  int savelen = 0;
+  auto Save = [&emu, &outmovie, &savestate, &savelen]() {
+      savelen = outmovie.size();
+      savestate = emu->SaveUncompressed();
+    };
+
+  auto Restore = [&emu, &savestate, &savelen, &outmovie]() {
+      outmovie.resize(savelen);
+      emu->LoadUncompressed(savestate);
+    };
+
+  Save();
+
+  // Now, repeatedly...
+
+  // This starts at 2 because the current and next count as drops.
+  uint8 prev_counter = 0x02;
+  int retry_count = 0;
+  int schedule_idx = 0;
+  int64 frame = 0;
+  int pieces = 0;
+  for (;;) {
+    // (note this is not really a frame counter currently, as it
+    // does not get saved/restored)
+    frame++;
+
+    uint8 cur = emu->ReadRAM(MEM_CURRENT_PIECE);
+    uint8 next_byte = emu->ReadRAM(MEM_NEXT_PIECE);
+    Piece next = DecodePiece(next_byte);
+    uint8 current_counter = emu->ReadRAM(MEM_DROP_COUNT);
+
+    #if 0
+    printf("%d frames sched %d. prev %d cur %d. cur %02x next %02x\n",
+           (int)outmovie.size(), schedule_idx,
+           prev_counter, current_counter, cur, next_byte);
+    #endif
+
+    if (cur == 0x13) {
+      // Wait for lines to finish. Not a valid piece so we don't
+      // want to enter the code below.
+      // TODO: tetrises?
+      emu->Step(0, 0);
+      outmovie.push_back(0);
+      continue;
+    }
+
+    if (prev_counter != current_counter) {
+      // A piece just landed.
+      // This means that 'cur' should just now have the next piece that
+      // we previously established as correct.
+      CHECK(DecodePiece(cur) == std::get<0>(schedule[(schedule_idx + 1) %
+                                                     schedule.size()]));
+
+      // Now check that the NEW next piece is what we expect.
+      const Piece expected =
+        std::get<0>(schedule[(schedule_idx + 2) % schedule.size()]);
+      if (next != expected) {
+        // We didn't get the expected piece.
+        // Go back to previous piece. Also keep track of how many
+        // times this has recently happened.
+        retry_count++;
+        if (retry_count > 64) {
+          static bool first = true;
+          if (first) SaveScreenshot("stuck.png", emu.get());
+          first = false;
+
+          const uint8 rng1 = emu->ReadRAM(MEM_RNG1);
+          const uint8 rng2 = emu->ReadRAM(MEM_RNG2);
+          printf("%d tries on piece %d (sched %d). cur: %02x=%c got next=%c want %c RNG %02x%02x fs %d\n",
+                 retry_count, current_counter, schedule_idx,
+                 cur, PieceChar(DecodePiece(cur)),
+                 PieceChar(DecodePiece(next_byte)),
+                 PieceChar(expected),
+                 rng1, rng2,
+                 (int)outmovie.size());
+        }
+        Restore();
+        continue;
+      }
+
+      // Otherwise, new checkpoint.
+      Save();
+      retry_count = 0;
+      schedule_idx++;
+      schedule_idx %= schedule.size();
+      pieces++;
+
+      prev_counter = current_counter;
+      if (pieces % 25 == 0) {
+        SaveScreenshot(StringPrintf("lucky%d.png", pieces),
+                       emu.get());
+
+        SimpleFM2::WriteInputs("lucky.fm2",
+                               "tetris.nes",
+                               "base64:Ww5XFVjIx5aTe5avRpVhxg==",
+                               outmovie);
+        printf("Saved after dropping %d pieces.\n", pieces);
+      }
+    }
+
+    uint8 cur_x = emu->ReadRAM(MEM_CURRENT_X);
+    uint8 cur_orientation = emu->ReadRAM(MEM_CURRENT_PIECE);
+    uint8 input = 0;
+
+    const auto [piece_, orientation, column] = schedule[schedule_idx];
+    CHECK(DecodePiece(cur) == piece_) <<
+      StringPrintf("Expect %c but have %c=%02x?",
+                   PieceChar(piece_),
+                   PieceChar(DecodePiece(cur)), cur);
+
+    if ((frame % 2) == 0) {
+      // PERF: Can rotate in the most efficient direction.
+      if (orientation != cur_orientation)
+        input |= INPUT_A;
+
+      if (column < cur_x) input |= INPUT_L;
+      else if (column > cur_x) input |= INPUT_R;
+    }
+
+    if (orientation == cur_orientation &&
+        column == cur_x) {
+      // printf("OK %d %d\n", orientation, column);
+      input = rc.Byte() & INPUT_D;
+      // If we're having trouble, even try pausing
+      if (retry_count > 64) input |= rc.Byte() & INPUT_T;
+    }
+
+    /*
+    printf("Put %c in orientation %02x (now %02x) in column %d (now %d) "
+           "input %s\n",
+           PieceChar(piece_), orientation, cur_orientation,
+           column, cur_x, SimpleFM2::InputToString(input).c_str());
+    */
+
+    emu->Step(input, 0);
+    outmovie.push_back(input);
+  }
+}
+
+
+[[maybe_unused]]
 static void HistoSimulate(int iters) {
   std::vector<int> histo(NUM_PIECES * NUM_PIECES, 0);
   std::vector<int> example(NUM_PIECES * NUM_PIECES, -1);
@@ -747,6 +958,158 @@ static void HistoSimulatedRNG() {
   histo.Save("simulated.png");
 }
 
+static const vector<std::tuple<float, float, float, float>> rgb_ramp = {
+  { 0.0f,  1.0f, 0.0f, 0.0f},
+  { 0.25f, 1.0f, 1.0f, 0.0f},
+  { 0.5f,  0.0f, 1.0f, 0.0f},
+  { 0.75f, 0.0f, 1.0f, 1.0f},
+  { 1.0f,  0.0f, 0.0f, 1.0f},
+};
+
+static std::tuple<float, float, float>
+LinearRamp(float t,
+           const vector<std::tuple<float, float, float, float>> &ramp) {
+  CHECK(!ramp.empty());
+  auto prev = ramp[0];
+
+  {
+    const auto [x, r, g, b] = prev;
+    if (t < x) {
+      return make_tuple(r, g, b);
+    }
+  }
+
+  for (int i = 1; i < (int)ramp.size(); i++) {
+    const auto now = ramp[i];
+    const auto [px, pr, pg, pb] = prev;
+    const auto [x, r, g, b] = now;
+    if (t < x) {
+     // linear interpolation
+      const float w = x - px;
+      const float f = (t - px) / w;
+      const float omf = 1.0f - f;
+      return make_tuple(f * r + omf * pr,
+                        f * g + omf * pg,
+                        f * b + omf * pb);
+    }
+    prev = now;
+  }
+
+  {
+    const auto [x, r, g, b] = prev;
+    return make_tuple(r, g, b);
+  }
+}
+
+// hard to read plot of the cycle
+[[maybe_unused]]
+static void PlotSimulatedRNG() {
+  constexpr int PERIOD = 32767;
+  constexpr int SCALE = 7;
+  ImageRGBA img(SCALE * 256, SCALE * 256);
+  img.Clear32(0x000000FF);
+  State state;
+
+  auto C = [](float f) -> uint8 {
+      return std::clamp(std::roundf(f * 255.0f), 0.0f, 255.0f);
+    };
+
+  int prev_x = state.rng1 * SCALE + SCALE / 2;
+  int prev_y = state.rng2 * SCALE + SCALE / 2;
+  for (int iters = 0; iters < PERIOD; iters++) {
+    state = NextRNG(state);
+    double f = iters / (double)PERIOD;
+    const auto [rf, gf, bf] = LinearRamp(f, rgb_ramp);
+    int x = state.rng1 * SCALE + SCALE / 2;
+    int y = state.rng2 * SCALE + SCALE / 2;
+
+    img.BlendLineAA(prev_x, prev_y, x, y,
+                    C(rf), C(gf), C(bf), 0x33);
+
+    prev_x = x;
+    prev_y = y;
+  }
+
+  const State start_state;
+  CHECK(state.rng1 == start_state.rng1 &&
+        state.rng2 == start_state.rng2);
+
+  img.Save("simulated-plot.png");
+}
+
+[[maybe_unused]]
+static void HeatSimulatedRNG() {
+  constexpr int PERIOD = 32767;
+  constexpr int SCALE = 7;
+  ImageRGBA img(256, 256);
+  img.Clear32(0x000000FF);
+  State state;
+
+  auto C = [](float f) -> uint8 {
+      return std::clamp(std::roundf(f * 255.0f), 0.0f, 255.0f);
+    };
+
+  for (int iters = 0; iters < PERIOD; iters++) {
+    state = NextRNG(state);
+    double f = iters / (double)PERIOD;
+    const auto [rf, gf, bf] = LinearRamp(f, rgb_ramp);
+    int x = state.rng1;
+    int y = state.rng2;
+
+    img.SetPixel(x, y, C(rf), C(gf), C(bf), 0xFF);
+  }
+
+  const State start_state;
+  CHECK(state.rng1 == start_state.rng1 &&
+        state.rng2 == start_state.rng2);
+
+  img.ScaleBy(SCALE).Save("simulated-heat.png");
+}
+
+[[maybe_unused]]
+static void PrintSequence() {
+  constexpr int PERIOD = 32767;
+  State state;
+
+  for (int iters = 0; iters < PERIOD; iters++) {
+    printf("%d. %d %d\n", iters, state.rng1, state.rng2);
+    state = NextRNG(state);
+  }
+
+  const State start_state;
+  CHECK(state.rng1 == start_state.rng1 &&
+        state.rng2 == start_state.rng2);
+}
+
+[[maybe_unused]]
+static void HeatPair() {
+  constexpr int PERIOD = 32767;
+  constexpr int SCALE = 7;
+  ImageRGBA img(256 * SCALE, 256 * SCALE);
+  img.Clear32(0x000000FF);
+  State state;
+
+  int prev = state.rng1;
+  for (int iters = 0; iters < PERIOD; iters++) {
+    state = NextRNG(state);
+    int cur = state.rng1;
+
+    img.BlendBox32(cur * SCALE + 1, prev * SCALE + 1,
+                   SCALE - 2, SCALE - 2,
+                   0xFFFFFFFF, 0x777777FF);
+    img.BlendRect32(cur * SCALE + 2, prev * SCALE + 2,
+                    SCALE - 4, SCALE -4, 0xFFFFFFFF);
+    prev = cur;
+  }
+
+  const State start_state;
+  CHECK(state.rng1 == start_state.rng1 &&
+        state.rng2 == start_state.rng2);
+
+  img.Save("heat-pair.png");
+}
+
+
 // Why do I get a checkerboard pattern for the NES
 // version if I don't play any inputs at start, and
 // the same for my transcribed code -- but get a
@@ -768,17 +1131,142 @@ static void HistoSimulatedRNG() {
 // - hmm yes that seems to be true, as this phenomenon
 //   goes away during gameplay.
 
+// Even though the first byte takes on all possible values and "looks
+// random", the dual-roll approach in NextPiece is not well
+// distributed:
+//
+// Given an rng byte, only two values are possible for the next byte,
+// (rng >> 1) and (128 + rng >> 1).
+//
+// In the re-roll scenario, we have that (rng + drop_count) & 0b111 is
+// either 0b111 or the last drop index. The next roll is one of
+// ((rng >> 1) + drop_count) & 0b111  or
+// (128 + (rng >> 1) + drop_count) & 0b111. These expressions are
+// congruent mod 8, so we really just have
+// ((rng >> 1) + drop_count) & 0b111.
+//  (could complete this analytical approach but it's also easy
+//   to just sample all states!)
+//
+// As a result (see AllRerolls), even if you have complete control
+// over the RNG state (but not prev piece and drop count), you
+// have at most 3 different outcomes possible in the reroll case.
+// For some setups, there is only 1 (rare) or 2 possible rerolls.
+// If one of those isn't the drop_piece, too bad! This happens in
+// 14.29% of cases for most pieces, except Z where it is 10.27%.
+//
+// So our easiest strategies would be ones where we alternate pieces,
+// since in any state we can get a piece different from the last drop
+// on the first roll. Another possibility would be to detect the 14%
+// of cases where it is impossible to continue the streak of our
+// desired piece (this can be a table or even analytical?) and instead
+// manage to roll a garbage piece in those cases that we have some
+// strategy for dealing with.
+
 // The only place that writes these bytes outside of
 // initialization really appears to be the RNG
 // routine. (Use "Forbid" breakpoint range on the
 // RNG code, in addition to a "write" breakpoint
 // on the rng state.)
 
+
+struct TinySet {
+  // Perhaps a faster data structure since max 3 items!
+  std::unordered_set<int> items;
+  void Add(int item) {
+    items.insert(item);
+    if (items.size() > 3) {
+      printf("Too many items for %d,%d:\n", z, last_piece);
+      for (int item : items)
+        printf("%d, ", item);
+      LOG(FATAL) << "just added " << item;
+    }
+  }
+
+  TinySet(int z, int last_piece) : z(z), last_piece(last_piece) {}
+  const int z, last_piece;
+};
+
+// TODO: Worst case "doughts" for getting a certain piece in a state?
+// It could be that rng1 stays even for half the period, as one simple
+// example. This would be 8 minutes of real time at 60hz!
+[[maybe_unused]]
+static void AllRerolls() {
+  constexpr std::array<uint8, 8> PIECES = {
+    0x02, 0x07, 0x08, 0x0A, 0x0B, 0x0E, 0x12,
+    // not used ?
+    0x02,
+  };
+
+  // for any counter / drop
+  int histo_states[4] = {};
+  int max_piece[7] = {};
+  int min_piece[7] = {9, 9, 9, 9, 9, 9, 9};
+  int cant_dup[7] = {};
+  for (uint8 last_drop = 0; last_drop < 7; last_drop++) {
+    for (int z = 0; z < 256; z++) {
+      const uint8 tmp_drop_count = z;
+
+      // show that the random state doesn't even matter
+
+      TinySet tes(z, last_drop);
+
+      for (int x = 0; x < 256; x++) {
+        const uint8 tmp_rng1 = x;
+        const uint8 first_roll = (tmp_rng1 + (tmp_drop_count + 1)) & 0x7;
+
+        if (first_roll == last_drop || first_roll == 0x7) {
+          // These are the re-roll scenarios.
+          // Try with all possible RNG states.
+          for (int y = 0; y < 256; y++) {
+            if (x == 0 && y == 0) continue;
+
+            State state;
+            state.rng1 = x;
+            state.rng2 = y;
+            state.drop_count = z;
+            state.last_drop = last_drop;
+            State after = NextPiece(state);
+
+            tes.Add(after.last_drop);
+          }
+        }
+      }
+
+      histo_states[tes.items.size()]++;
+      max_piece[last_drop] = std::max(max_piece[last_drop],
+                                      (int)tes.items.size());
+      min_piece[last_drop] = std::min(min_piece[last_drop],
+                                      (int)tes.items.size());
+
+      if (tes.items.find(last_drop) == tes.items.end())
+        cant_dup[last_drop]++;
+    }
+  }
+  printf("OK\n");
+  printf("Count with 0: %d, 1: %d, 2: %d, 3: %d\n",
+         histo_states[0], histo_states[1], histo_states[2], histo_states[3]);
+
+  for (int p = 0; p < 7; p++) {
+    char pc = PieceChar(DecodePiece(PIECES[p]));
+    printf("Best case for piece %d=%c: %d, worst: %d, can't dup: %d/%d = %.02f%%\n",
+           p, pc, max_piece[p], min_piece[p], cant_dup[p],
+           256 * 7, (100.0 * cant_dup[p]) / (256 * 7));
+  }
+}
+
 int main(int argc, char **argv) {
   // HistoMenu(1024);
   // HistoSimulate(65536);
-  HistoRNG();
-  HistoSimulatedRNG();
+  // HistoRNG();
+  // HistoSimulatedRNG();
+
+  // PlotSimulatedRNG();
+  // HeatSimulatedRNG();
+  // PrintSequence();
+  // HeatPair();
+
+  // AllRerolls();
+  MakeStreak2();
 
   return 0;
 }
