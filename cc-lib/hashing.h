31196d57088b1e0b03db30b3908ed62204c01a16
Tom7 <499244+tom7@users.noreply.github.com>
2022-02-21 03:31:31 +0000

    Add Hashing for vector<>, missing include guard
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4560 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- cc-lib/hashing.h
+++ cc-lib/hashing.h
@@ -1,7 +1,9 @@
 
-// Generic hashing for use in e.g. std::unordered_map. It is not
-// allowed to extend std::hash (or anything in std::) unless the
-// declaration involves a user-defined type, so the absence of
+// Generic hashing for use in e.g. std::unordered_map. Not guaranteed to
+// be stable over time.
+//
+// It is not allowed to extend std::hash (or anything in std::) unless
+// the declaration involves a user-defined type, so the absence of
 // std::hash on containers like std::pair is pretty annoying.
 //
 // Use like:
@@ -9,11 +11,14 @@
 //     std::unordered_map<std::pair<int, std::string>, int,
 //                        Hashing<std::pair<int, std::string>>;
 
+#ifndef _CC_LIB_HASHING_H
+#define _CC_LIB_HASHING_H
+
 #include <utility>
 #include <functional>
 
 namespace hashing_internal {
-static constexpr inline RotateSizeT(size_t v, int bits) {
+static constexpr inline std::size_t RotateSizeT(size_t v, int bits) {
   return (v << bits) | (v >> (sizeof v * 8 - bits));
 }
 }
@@ -37,3 +42,19 @@ struct Hashing<std::pair<T, U>> {
     return th + 0x9e3779b9 + hashing_internal::RotateSizeT(uh, 15);
   }
 };
+
+// PERF: Probably should at least specialize for numeric types like
+// uint8_t.
+template<class T>
+struct Hashing<std::vector<T>> {
+  std::size_t operator()(const std::vector<T> &v) const {
+	size_t h = 0xCAFED00D + v.size();
+	for (const T &t : v) {
+	  h += Hashing<T>()(t);
+	  h = hashing_internal::RotateSizeT(h, 13);
+	}
+	return h;
+  }
+};
+
+#endif
