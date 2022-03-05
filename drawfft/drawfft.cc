cc0ee7b170057777e1dec80e26590c7a8fd00a46
Tom7 <499244+tom7@users.noreply.github.com>
2022-03-05 17:08:20 +0000

    Fix makefile (-I/usr/local/include is toxic), incorporate chars.h
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4575 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- drawfft/drawfft.cc
+++ drawfft/drawfft.cc
@@ -1,5 +1,6 @@
 
 #include <string>
+
 #include <vector>
 #include <shared_mutex>
 #include <cstdint>
@@ -19,6 +20,7 @@
 #include "SDL_main.h"
 #include "../cc-lib/sdl/sdlutil.h"
 #include "../cc-lib/sdl/font.h"
+#include "../cc-lib/sdl/chars.h"
 #include "../cc-lib/sdl/cursor.h"
 #include "../cc-lib/lines.h"
 #include "../cc-lib/util.h"
@@ -27,46 +29,11 @@
 
 #include <api/fftw3.h>
 
-/* there are some non-ascii symbols in the font */
-#define CHECKMARK "\xF2"
-#define ESC "\xF3"
-#define HEART "\xF4"
-/* here L means "long" */
-#define LCMARK1 "\xF5"
-#define LCMARK2 "\xF6"
-#define LCHECKMARK LCMARK1 LCMARK2
-#define LRARROW1 "\xF7"
-#define LRARROW2 "\xF8"
-#define LRARROW LRARROW1 LRARROW2
-#define LLARROW1 "\xF9"
-#define LLARROW2 "\xFA"
-#define LLARROW LLARROW1 LLARROW2
-
-/* BAR_0 ... BAR_10 are guaranteed to be consecutive */
-#define BAR_0 "\xE0"
-#define BAR_1 "\xE1"
-#define BAR_2 "\xE2"
-#define BAR_3 "\xE3"
-#define BAR_4 "\xE4"
-#define BAR_5 "\xE5"
-#define BAR_6 "\xE6"
-#define BAR_7 "\xE7"
-#define BAR_8 "\xE8"
-#define BAR_9 "\xE9"
-#define BAR_10 "\xEA"
-#define BARSTART "\xEB"
-
-#define FONTCHARS " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`-=[]\\;',./~!@#$%^&*()_+{}|:\"<>?" CHECKMARK ESC HEART LCMARK1 LCMARK2 BAR_0 BAR_1 BAR_2 BAR_3 BAR_4 BAR_5 BAR_6 BAR_7 BAR_8 BAR_9 BAR_10 BARSTART LRARROW LLARROW
-
-#define FONTSTYLES 7
-
 using namespace std;
 
 using int64 = int64_t;
 using uint32 = uint32_t;
 
-#define FONTWIDTH 9
-#define FONTHEIGHT 16
 static Font *font = nullptr, *font2x = nullptr, *font4x = nullptr;
 
 static SDL_Cursor *cursor_arrow = nullptr, *cursor_bucket = nullptr;
