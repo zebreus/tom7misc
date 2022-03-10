f9ebe21f725d18d8911ce91abf6e687a5d431720
Tom7 <499244+tom7@users.noreply.github.com>
2022-03-10 04:00:26 +0000

    Support group sel and 6-bit addresses for new board. works
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4583 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- pingu/ee/setdemux.cc
+++ pingu/ee/setdemux.cc
@@ -14,19 +14,42 @@ using uint8 = uint8_t;
 using uint32 = uint32_t;
 using namespace std;
 
-// Three bits at 22, 23, 24
+// Six bits at 22, 23, 24, 25, 26, 27
 static constexpr int DEMUX_GPIO = 22;
+
+static constexpr int GROUP_SEL_A = 17;
+static constexpr int GROUP_SEL_B = 16;
+
+// XXX make configurable
+static constexpr bool USE_GROUP_A = true;
 
 static void SetDemux(uint8 value) {
   CHECK_GE(value, 0) << value;
-  CHECK_LT(value, 8) << value;
+  CHECK_LT(value, 64) << value;
 
-  for (int i = 0; i < 3; i++) {
+  {
+    // This is hard-coded, but we need to set the group sel bits to
+    // 10 or 01.
+    bcm2835_gpio_fsel(GROUP_SEL_A, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_set_pud(GROUP_SEL_A, BCM2835_GPIO_PUD_OFF);
+    bcm2835_gpio_fsel(GROUP_SEL_B, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_set_pud(GROUP_SEL_B, BCM2835_GPIO_PUD_OFF);
+
+    constexpr uint32 GROUP_MASK =
+      (1 << GROUP_SEL_A) | (1 << GROUP_SEL_B);
+    if (USE_GROUP_A) {
+      bcm2835_gpio_write_mask((1 << GROUP_SEL_A), GROUP_MASK);
+    } else {
+      bcm2835_gpio_write_mask((1 << GROUP_SEL_B), GROUP_MASK);
+    }
+  }
+  
+  for (int i = 0; i < 6; i++) {
     bcm2835_gpio_fsel(DEMUX_GPIO + i, BCM2835_GPIO_FSEL_OUTP);
     bcm2835_gpio_set_pud(DEMUX_GPIO + i, BCM2835_GPIO_PUD_OFF);
   }
     
-  bcm2835_gpio_write_mask((value << DEMUX_GPIO), (0b111 << DEMUX_GPIO));
+  bcm2835_gpio_write_mask((value << DEMUX_GPIO), (0b111111 << DEMUX_GPIO));
   printf("Set demux to %d.\n", value);
 
   return;
