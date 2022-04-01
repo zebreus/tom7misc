f83fbb37afdc2d3f31b9ef4e3ea6d09988ce3297
Tom7 <499244+tom7@users.noreply.github.com>
2022-04-01 22:16:28 +0000

    Add sars-cov-2 sequence and bit-packing program
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4604 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

---
+++ pingu/makesars.cc
@@ -0,0 +1,62 @@
+
+#include <string>
+#include <cstdio>
+
+#include "base/logging.h"
+#include "util.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  string rawf = Util::ReadFile("sars.txt");
+  CHECK(!rawf.empty());
+
+  string s;
+  for (char c : rawf) {
+    switch (c) {
+    case 'a': s.push_back('a');
+    case 'c': s.push_back('c');
+    case 't': s.push_back('u');  // This is actually uracil
+    case 'g': s.push_back('g');
+    case ' ':
+    case '\n':
+    case '\r':
+      break;
+    default:
+      LOG(FATAL) << "Unexpected character: " << c;
+    }
+  }
+
+  printf("Base pairs: %d\n", (int)s.size());
+
+  // We have to store in bytes, so pad with zero (a).
+  while ((s.size() % 4) != 0) {
+    s.push_back('a');
+  }
+  
+  std::vector<uint8_t> bin;
+  auto Bits = [](char c) {
+    switch (c) {
+    case 'a': return 0b00;
+    case 'c': return 0b01;
+    case 'g': return 0b10;
+    case 'u': return 0b11;
+    default:
+      LOG(FATAL) << "Unencodable: " << c;
+      return 0;
+    }
+    };
+
+  CHECK((s.size() % 4) == 0);
+  for (int i = 0; i < (int)s.size(); i += 4) {
+    const uint8_t b = (Bits(s[0]) << 6) |
+      (Bits(s[1]) << 4) |
+      (Bits(s[2]) << 2) |
+      (Bits(s[3]) << 0);
+    bin.push_back(b);
+  }
+
+  printf("Binary size: %d\n", (int)bin.size());
+  CHECK(Util::WriteFileBytes("sars.bin", bin));
+  return 0;
+}
