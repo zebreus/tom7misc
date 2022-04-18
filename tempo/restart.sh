c7bf02e363c6fe4f5740580d997fa8fc12bcd087
Tom7 <499244+tom7@users.noreply.github.com>
2022-04-18 22:48:14 +0000

    Just use one thread to compile in restart, since I experienced some OOMs recently?
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4611 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- tempo/restart.sh
+++ tempo/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/pi/tom7misc/tempo && make -j 2 tempo.exe
+cd /home/pi/tom7misc/tempo && make tempo.exe
 sudo killall tempo.exe
 cd /home/pi/tom7misc/tempo && sudo nohup ./tempo.exe > /dev/null 2>&1 &
 disown
