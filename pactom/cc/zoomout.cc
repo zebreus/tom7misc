ff5021dc936e1eb5f1d6976dc1136eaf744d91ef
Tom7 <499244+tom7@users.noreply.github.com>
2022-05-28 15:41:25 +0000

    Zoom and animate, nice
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4618 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

---
+++ pactom/cc/zoomout.cc
@@ -0,0 +1,173 @@
+
+#include "pactom.h"
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <cstdint>
+
+#include "base/logging.h"
+#include "base/stringprintf.h"
+#include "geom/latlon.h"
+#include "bounds.h"
+#include "image.h"
+#include "lines.h"
+#include "arcfour.h"
+#include "randutil.h"
+#include "color-util.h"
+#include "threadutil.h"
+
+using namespace std;
+
+
+using int64 = int64_t;
+
+static constexpr int WIDTH = 1920;
+static constexpr int HEIGHT = 1080;
+static constexpr int SCALE = 4;
+// Additional pixels to draw for line (0 = 1 pixel thick)
+static constexpr int RADIUS = 2;
+
+static constexpr int NUM_FRAMES = 256;
+
+static uint32 RandomBrightColor(ArcFour *rc) {
+  const float h = RandFloat(rc);
+  const float s = 0.5f + (0.5f * RandFloat(rc));
+  const float v = 0.5f + (0.5f * RandFloat(rc));
+  float r, g, b;
+  ColorUtil::HSVToRGB(h, s, v, &r, &g, &b);
+  const uint32 rr = std::clamp((int)roundf(r * 255.0f), 0, 255);
+  const uint32 gg = std::clamp((int)roundf(g * 255.0f), 0, 255);
+  const uint32 bb = std::clamp((int)roundf(b * 255.0f), 0, 255);
+
+  return (rr << 24) | (gg << 16) | (bb << 8) | 0xFF;
+}
+
+int main(int argc, char **argv) {
+  ArcFour rc("pactom");
+  unique_ptr<PacTom> pactom = PacTom::FromFiles({"../pac.kml",
+                                                 "../pac2.kml"},
+    "../neighborhoods.kml"
+    );
+  CHECK(pactom.get() != nullptr);
+
+  int64 pts = 0;
+  for (auto &p : pactom->paths) pts += p.size();
+
+  printf("Loaded %lld paths with %lld waypoints.\n",
+         pactom->paths.size(), pts);
+  printf("There are %d hoods\n", pactom->hoods.size());
+
+  std::vector<std::pair<uint32_t,
+                        std::vector<std::pair<LatLon, double>>>> paths;
+  paths.reserve(pactom->paths.size());
+  for (const auto &p : pactom->paths) {
+    uint32_t color = RandomBrightColor(&rc) & 0xFFFFFF33; // XXX
+    paths.emplace_back(color, p);
+  }
+
+  const LatLon home = LatLon::FromDegs(40.452911, -79.936313);
+  LatLon::Projection Project = LatLon::Gnomonic(home);
+  const std::pair<double, double> home_pt = Project(home);
+  // LatLon::Projection Project = LatLon::PlateCarree();
+
+  // Find the extrema.
+  Bounds bounds;
+  for (const auto &[color_, p] : paths) {
+    for (const auto &[latlon, elev] : p) {
+      auto [x, y] = Project(latlon);
+      bounds.Bound(x, y);
+    }
+  }
+  bounds.AddMarginFrac(0.05);
+
+  // The bounds for the full data are the endpoint.
+  Bounds::Scaler scaler_end = bounds.ScaleToFit(WIDTH * SCALE,
+                                                HEIGHT * SCALE).FlipY();
+
+  // Screen location of home.
+  auto home_screen = scaler_end.Scale(home_pt);
+
+  // Start scale; centered on home.
+  Bounds::Scaler scaler_start = scaler_end.
+    // put home at 0,0
+    PanScreen(-home_screen.first, -home_screen.second).
+    // zoom
+    Zoom(6.0, 6.0).
+    // center screen on 0, 0
+    PanScreen(WIDTH * SCALE * 0.5, HEIGHT * SCALE * 0.5);
+
+  auto ScaleInterp = [&](double f, std::pair<double, double> pt) ->
+    std::pair<double, double> {
+      auto [x0, y0] = scaler_start.Scale(pt);
+      auto [x1, y1] = scaler_end.Scale(pt);
+
+      return std::make_pair(f * x1 + (1.0 - f) * x0,
+                            f * y1 + (1.0 - f) * y0);
+    };
+
+  auto MakeFrame = [&](int64_t frame) {
+      const double frame_frac = frame / (double)(NUM_FRAMES - 1);
+
+      ImageRGBA image(WIDTH * SCALE, HEIGHT * SCALE);
+      image.Clear32(0x000000FF);
+
+      for (const auto &[name, path] : pactom->hoods) {
+        constexpr uint32 color = 0x909090FF;
+        for (int i = 0; i < path.size() - 1; i++) {
+          const LatLon latlon0 = path[i];
+          const LatLon latlon1 = path[i + 1];
+          auto [x0, y0] = ScaleInterp(frame_frac, Project(latlon0));
+          auto [x1, y1] = ScaleInterp(frame_frac, Project(latlon1));
+
+          for (const auto [x, y] :
+                 Line<int>{(int)x0, (int)y0, (int)x1, (int)y1}) {
+            for (int dy = -RADIUS; dy <= RADIUS; dy++) {
+              const int ddy = dy * dy;
+              for (int dx = -RADIUS; dx <= RADIUS; dx++) {
+                const int ddx = dx * dx;
+                if (ddy + ddx <= RADIUS * RADIUS) {
+                  image.BlendPixel32(x + dx, y + dy, color);
+                }
+              }
+            }
+          }
+
+        }
+      }
+
+      for (const auto &[color, p] : paths) {
+        const int last_pt =
+          std::clamp((int)std::round(p.size() * frame_frac), 0, (int)p.size());
+        for (int i = 0; i < last_pt - 1; i++) {
+          const auto &[latlon0, elev0] = p[i];
+          const auto &[latlon1, elev1] = p[i + 1];
+          auto [x0, y0] = ScaleInterp(frame_frac, Project(latlon0));
+          auto [x1, y1] = ScaleInterp(frame_frac, Project(latlon1));
+
+          for (const auto [x, y] :
+                 Line<int>{(int)x0, (int)y0, (int)x1, (int)y1}) {
+            for (int dy = -RADIUS; dy <= RADIUS; dy++) {
+              const int ddy = dy * dy;
+              for (int dx = -RADIUS; dx <= RADIUS; dx++) {
+                const int ddx = dx * dx;
+                if (ddy + ddx <= RADIUS * RADIUS) {
+                  image.BlendPixel32(x + dx, y + dy, color);
+                }
+              }
+            }
+          }
+
+        }
+      }
+
+      ImageRGBA out = image.ScaleDownBy(SCALE);
+
+      out.Save(StringPrintf("zoom%04d.png", (int)frame));
+      printf(".");
+    };
+
+  ParallelComp(NUM_FRAMES, MakeFrame, 8);
+
+  return 0;
+}
