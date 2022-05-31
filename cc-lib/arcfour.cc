e602e74a853dbfd4407d31fae3dd60da1bed3751
Tom7 <499244+tom7@users.noreply.github.com>
2022-05-31 03:32:00 +0000

    Remove unused strings/. Add benchmark for arcfour and improve the speed of initialization a little.
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4627 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- cc-lib/arcfour.cc
+++ cc-lib/arcfour.cc
@@ -10,44 +10,34 @@ using namespace std;
 using uint8 = uint8_t;
 static_assert (sizeof(uint8) == 1, "Char must be one byte.");
 
-static void Initialize(const uint8 (&kk)[256],
+template<class K>
+static void Initialize(K k,
                        uint8 (&ss)[256]) {
-  uint8 i = 0, j = 0;
-  for (int n = 256; n--;) {
-    j += ss[i] + kk[i];
+  uint8 j = 0;
+  for (int i = 0; i < 256; i++) ss[i] = i;
+  for (int i = 0; i < 256; i++) {
+    j += ss[i] + k(i);
     uint8 t = ss[i];
     ss[i] = ss[j];
     ss[j] = t;
-    i++;
   }
 }
 
 ArcFour::ArcFour(const vector<uint8> &v) : ii(0), jj(0) {
-  uint8 kk[256];
-  for (int i = 0; i < 256; i++) {
-    ss[i] = i;
-    kk[i] = v[i % v.size()];
-  }
-  Initialize(kk, ss);
+  Initialize([&v](int i) { return v[i % v.size()]; }, ss);
 }
 
 ArcFour::ArcFour(const string &s) : ii(0), jj(0) {
-  uint8 kk[256];
-  for (int i = 0; i < 256; i++) {
-    ss[i] = i;
-    kk[i] = (uint8)s[i % s.size()];
-  }
-  Initialize(kk, ss);
+  Initialize([&s](int i) { return s[i % s.size()]; }, ss);
 }
 
-uint8 ArcFour::Byte() {
+uint8_t ArcFour::Byte() {
   ii++;
   jj += ss[ii];
-  uint8 ti = ss[ii];
-  uint8 tj = ss[jj];
+  uint8_t ti = ss[ii];
+  uint8_t tj = ss[jj];
   ss[ii] = tj;
   ss[jj] = ti;
-
   return ss[(ti + tj) & 255];
 }
 
