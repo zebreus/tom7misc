d7bb84ab78ab9b3bfe2ee047940a248a184830ce
Tom7 <499244+tom7@users.noreply.github.com>
2022-06-08 01:51:41 +0000

    Add MapTuple
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4630 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

---
+++ cc-lib/tuple-util.h
@@ -0,0 +1,26 @@
+
+#ifndef _CC_LIB_TUPLE_UTIL_H
+#define _CC_LIB_TUPLE_UTIL_H
+
+#include <tuple>
+#include <utility>
+#include <cstdint>
+
+namespace internal {
+template<class F, class T, size_t... I>
+auto MapTupleInternal(const F &f,
+                      const T &t,
+                      std::index_sequence<I...>) {
+  // Expands the expression for each int.
+  return std::make_tuple(f(std::get<I>(t))...);
+}
+}
+
+template<class F, class... TS>
+auto MapTuple(const F &f,
+              const std::tuple<TS...> &t) {
+  return internal::MapTupleInternal(
+      f, t, std::make_index_sequence<sizeof...(TS)>{});
+}
+
+#endif
