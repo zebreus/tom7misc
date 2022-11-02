a1b0af93fd60c4ef695dedfcc54786879adc2827
Tom7 <499244+tom7@users.noreply.github.com>
2022-11-02 22:13:00 +0000

    Shade, rotate, add cutout
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4740 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- pactom/cc/sphere.cc
+++ pactom/cc/sphere.cc
@@ -11,8 +11,7 @@
 
 static constexpr int THREADS = 24;
 
-static constexpr double EARTH_RADIUS = 1.0f;
-static constexpr double FINAL_SCREEN_DISTANCE = 0.0f;
+static constexpr double EARTH_RADIUS = 2.8f;
 
 static constexpr double FAR_WIDTH = 100.0f;
 // static constexpr double NEAR_WIDTH = 0.01f;
@@ -28,12 +27,81 @@ static constexpr int FRAME_HEIGHT = 1080;
 
 using namespace yocto;
 
+static ImageRGBA *bluemarble = nullptr;
+
+mat3f RotYaw(double a) {
+  const double cosa = cos(a);
+  const double sina = sin(a);
+  return mat3f
+    {cosa, -sina, 0.0,
+     sina, cosa, 0.0,
+     0.0, 0.0, 1.0};
+}
+
+mat3f RotPitch(double a) {
+  const double cosa = cos(a);
+  const double sina = sin(a);
+
+  return mat3f
+    {cosa, 0.0, sina,
+     0.0, 1.0, 0.0,
+     -sina, 0.0, cosa};
+}
+
+mat3f RotRoll(double a) {
+  const double cosa = cos(a);
+  const double sina = sin(a);
+
+  return mat3f
+    {1.0, 0.0, 0.0,
+     0.0, cosa, -sina,
+     0.0, sina, cosa};
+}
+
+mat3f Rot(double yaw, double pitch, double roll) {
+  mat3f mr = RotRoll(roll);
+  mat3f mp = RotPitch(pitch);
+  mat3f my = RotYaw(yaw);
+  mat3f m = mp * my;
+  mat3f n = mr * m;
+  return n;
+}
+
+struct Tetrahedron {
+  vec3f p0, p1, p2, p3;
+};
+
+// TODO: Doesn't tell us which face it hit; same UV coordinates no matter
+// what.
+inline prim_intersection intersect_tetrahedron(
+    const ray3f& ray, const Tetrahedron &tet) {
+
+  //    1---3
+  //   / \ /
+  //  0---2
+
+  // TODO: These all have the same winding order, but probably we want
+  // to pick them so that they have consistent UV coordinates or something.
+  prim_intersection p0 = intersect_triangle(ray, tet.p0, tet.p1, tet.p2);
+  prim_intersection p1 = intersect_triangle(ray, tet.p1, tet.p3, tet.p2);
+  prim_intersection p2 = intersect_triangle(ray, tet.p0, tet.p3, tet.p1);
+  prim_intersection p3 = intersect_triangle(ray, tet.p2, tet.p3, tet.p0);
+
+  // take the closest one
+  for (const auto &p : {p1, p2, p3}) {
+    if (p.hit && p.distance < p0.distance) p0 = p;
+  }
+
+  return p0;
+}
+
+
 static ImageRGBA RenderFrame(
     // Distance from Earth
     double distance,
     // Rotation of Earth along its axis
     double angle) {
-  constexpr int OVERSAMPLE = 3;
+  constexpr int OVERSAMPLE = 2;
 
   ImageRGBA img(FRAME_WIDTH * OVERSAMPLE, FRAME_HEIGHT * OVERSAMPLE);
 
@@ -58,7 +126,7 @@ static ImageRGBA RenderFrame(
         float far_dist = 100.0f;
         // (maybe we want much less to zoom to a
         // region on the planet?)
-        float near_dist = -10.1f;
+        float near_dist = -20.1f;
 
         float near_x = std::lerp(-NEAR_WIDTH * 0.5, NEAR_WIDTH * 0.5, xf);
         float near_y = std::lerp(-NEAR_HEIGHT * 0.5, NEAR_HEIGHT * 0.5, yf);
@@ -76,24 +144,55 @@ static ImageRGBA RenderFrame(
         ray.tmin = 0.001f;
         ray.tmax = 10.0f;
 
+        // Default orientation would be facing the south pole.
+        // Rotate...
+        // double axis = 0.0;
+        mat3f rot = Rot(0.0, 3.14159 + 0.25, 3.14159 * 0.5);
+        // Rotate around origin.
+        frame3f rot_frame = make_frame(rot, {0.0, 0.0, 0.0});
+        ray = transform_ray(rot_frame, ray);
+
         // test: intersect sphere
         /*
           vec2f uv       = {0, 0};
           float distance = flt_max;
           bool  hit      = false;
         */
-        prim_intersection isect =
+        prim_intersection s_isect =
           intersect_sphere(ray, {0.0, 0.0, 0.0}, EARTH_RADIUS);
 
-        if (isect.hit) {
-          float r = std::clamp(isect.uv.x, 0.0f, 1.0f);
-          float b = std::clamp(isect.uv.y, 0.0f, 1.0f);
+        if (s_isect.hit) {
+          #if 0
+          float r = std::clamp(s_isect.uv.x, 0.0f, 1.0f);
+          float b = std::clamp(s_isect.uv.y, 0.0f, 1.0f);
 
           img.SetPixel32(px, py,
                          ColorUtil::FloatsTo32(r, 0.0f, b, 1.0f));
+          #endif
+          uint32_t color = bluemarble->GetPixel32(
+              s_isect.uv.x * (double)bluemarble->Width(),
+              s_isect.uv.y * (double)bluemarble->Height());
+
+          img.SetPixel32(px, py, color);
 
         } else {
           img.SetPixel32(px, py, 0x111100FF);
+        }
+
+        vec3f tp1 = {0, -2.5, 0};
+        vec3f tp2 = {0, +2.5, 0};
+        vec3f tp3 = {5, 0, -5};
+        vec3f tp4 = {5, 0, +5};
+
+        Tetrahedron tet(tp1, tp2, tp3, tp4);
+        prim_intersection t_isect = intersect_tetrahedron(ray, tet);
+
+        if (t_isect.hit) {
+          float r = std::clamp(t_isect.uv.x, 0.0f, 1.0f);
+          float b = std::clamp(t_isect.uv.y, 0.0f, 1.0f);
+
+          img.BlendPixel32(px, py,
+                           ColorUtil::FloatsTo32(r, 0.0f, b, 0.2f));
         }
 
         // TODO:
@@ -108,8 +207,15 @@ static ImageRGBA RenderFrame(
 
 int main(int argc, char **argv) {
 
+  printf("Loading marble...\n");
+  // bluemarble = ImageRGBA::Load("world_shaded_43k.jpg");
+  bluemarble = ImageRGBA::Load("bluemarble.png");
+  CHECK(bluemarble != nullptr);
+  printf("Done.\n");
+
   ImageRGBA img = RenderFrame(100, 0);
   img.Save("sphere.png");
 
+  delete bluemarble;
   return 0;
 }
