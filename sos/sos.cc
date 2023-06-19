9040e235cf4f7cddf7de504d30900ec4fa8b6e1f
Tom7 <499244+tom7@users.noreply.github.com>
2023-06-19 13:07:15 +0000

    Faster enumeration of ways
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4900 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- sos/sos.cc
+++ sos/sos.cc
@@ -5,7 +5,6 @@
 #include <string>
 #include <bit>
 #include <tuple>
-#include <unordered_map>
 
 // #include "clutil.h"
 #include "base/logging.h"
@@ -17,6 +16,8 @@
 #include "periodically.h"
 #include "timer.h"
 #include "ansi.h"
+#include "autoparallel.h"
+#include "factorize.h"
 
 using namespace std;
 
@@ -25,6 +26,7 @@ using namespace std;
 using int64 = int64_t;
 
 // https://www.nuprl.org/MathLibrary/integer_sqrt/
+#if 0
 static uint64_t Sqrt64(uint64_t xx) {
   if (xx <= 1) return xx;
   // z = xx / 4
@@ -33,6 +35,15 @@ static uint64_t Sqrt64(uint64_t xx) {
   uint64_t r3 = r2 + 1;
   return (xx < r3 * r3) ? r2 : r3;
 }
+#else
+
+static uint64_t Sqrt64(uint64_t n) {
+  if (n == 0) return 0;
+  uint64_t r = std::sqrt((double)n);
+  return r - (r * r - 1 >= n);
+}
+
+#endif
 
 // Slow decomposition into sums of squares two ways, for reference.
 static optional<tuple<uint64_t,uint64_t,uint64_t,uint64_t>>
@@ -106,6 +117,65 @@ ReferenceValidate3(uint64_t sum) {
   return nullopt;
 }
 
+static string WaysString(const std::vector<std::pair<uint64_t, uint64_t>> &v) {
+  string out;
+  for (const auto &[a, b] : v) {
+    StringAppendF(&out, "%llu^2 + %llu^2, ", a, b);
+  }
+  return out;
+}
+
+// n ways.
+static std::vector<std::pair<uint64_t, uint64_t>>
+BruteGetNWays(uint64_t sum) {
+  // Neither factor can be larger than the square root.
+  // uint64_t limit_b = Sqrt64(sum);
+  // while (limit_b * limit_b < sum) limit_b++;
+  // Also, since a^2 + b^2 = sum, but a < b, a^2 can actually
+  // be at most half the square root.
+  uint64_t limit_a = Sqrt64(sum / 2);
+  while (limit_a * limit_a < (sum / 2)) limit_a++;
+
+  // XXX also since a < b, only need to go to half
+
+  // with x^2 + y^2 == sum and x <= y, get y.
+  // (Or return zero)
+  auto GetOther = [sum](uint64_t x) -> uint64_t {
+      uint64_t xx = x * x;
+      uint64_t target = sum - xx;
+      uint64_t y = Sqrt64(target);
+      // Insist that the result is smaller than the
+      // input, even if it would work. We find it the
+      // other way. Try x = 7072 for sum = 100012225.
+      if (y < x) return 0;
+      if (y * y == target) return y;
+      else return 0;
+      #if 0
+      for (uint64_t y = x; /* in loop */; y++) {
+        const uint64_t yy = y * y;
+        if (yy == target) {
+          return y;
+        } else if (yy > target) {
+          return 0;
+        }
+      }
+      return 0;
+      #endif
+    };
+
+  // The way we ensure distinctness is that the pairs are ordered
+  // a < b, and the search (and vector) is ordered by the first
+  // element.
+  std::vector<std::pair<uint64_t, uint64_t>> ret;
+  for (uint64_t a = 0; a <= limit_a; a++) {
+    if (uint64_t b = GetOther(a)) {
+      ret.emplace_back(a, b);
+    }
+  }
+
+  return ret;
+}
+
 
 // Some useful facts:
 //  Must be in OEIS https://oeis.org/A004431, sum of two distinct squares.
@@ -121,9 +191,8 @@ ReferenceValidate3(uint64_t sum) {
 // From note on OEIS by Chai Wah Wu, Sep 08 2022.
 static int ChaiWahWu(uint64_t sum) {
   if (sum == 0) return 1;
-  std::vector<int> factors = Util::Factorize(sum);
-  std::unordered_map<int, int> collated;
-  for (int f : factors) collated[f]++;
+  std::vector<std::pair<uint64_t, int>> collated =
+    Factorize::PrimeFactorization(sum);
 
   auto HasAnyOddPowers = [&collated]() {
       for (const auto &[p, e] : collated) {
@@ -158,10 +227,12 @@ static void TestCWW() {
   std::mutex m;
   int wrong = 0;
   Timer timer;
+  static constexpr int START = 1000000;
   static constexpr int NUM = 1000000;
   ParallelComp(
       NUM,
-      [&wrong, &m](int i) {
+      [&wrong, &m](int idx) {
+        int i = START + idx;
         int num = ChaiWahWu(i);
         auto fo = ReferenceValidate2(i);
         auto ffo = ReferenceValidate3(i);
@@ -205,12 +276,100 @@ static void TestCWW() {
   }
 }
 
+// 264.3/sec -> 24401.7/sec :)
+static void GenCWW() {
+  // AutoParallelComp comp(16, 1000, true, "cww.autoparallel");
+
+  std::mutex m;
+  int triples = 0;
+  Timer timer;
+  static constexpr uint64_t START = 100'000'000;
+  static constexpr uint64_t NUM   = 100'000'000;
+  Periodically status_per(10.0);
+  // comp.
+    ParallelComp(
+      NUM,
+      [&triples, &status_per, &timer, &m](uint64_t idx) {
+        uint64_t i = START + idx;
+        // This one doesn't work, and we don't care.
+        if (i == 0) return;
+
+        int num = ChaiWahWu(i);
+        std::vector<std::pair<uint64_t, uint64_t>> nways =
+          BruteGetNWays(i);
+        for (const auto &[a, b] : nways) {
+          CHECK(a * a + b * b == i) << a << " " << b << " " << i;
+        }
+        CHECK((int)nways.size() == num)
+          << "For sum " << i << ", CWW says "
+          << num
+          << " but got " << nways.size() << ":\n"
+          << WaysString(nways);
+
+        if (num > 3) {
+          MutexLock ml(&m);
+          triples++;
+          if (status_per.ShouldRun()) {
+            double pct = (triples * 100.0)/(double)idx;
+            double sec = timer.Seconds();
+            double nps = idx / sec;
+            printf("%d/%llu (%.5f%%) are triples (%s) %.1f/sec\n",
+                   triples, idx, pct, AnsiTime(sec).c_str(), nps);
+          }
+        }
+      }, 6);
+
+  double sec = timer.Seconds();
+  printf("Total triples: %d/%llu\n", triples, NUM);
+  printf("Done in %s. (%s/ea.)\n",
+         AnsiTime(sec).c_str(), AnsiTime(sec / NUM).c_str());
+}
+
+// So now take numbers that can be written as sums of squares
+// three ways: Z = B^2 + C^2 = D^2 + G^2 = E^2 + I^2
+//
+//  [a]  B   C
+//
+//   D   E  [f]
+//
+//   G  [h]  I
+//
+// This gives us the SUM = G + E + C, which then uniquely
+// determines a, f, h (if they exist). Since the starting
+// values were distinct, these residues are also distinct.
+//
+// The order of (B, C), (D, G), (E, I) matters, although there
+// are some symmetries. We can req
+
+// old, worse version
+//
+// So we have
+//  A B C
+//  D E F
+//  G H I
+//
+// For some choice of A,
+// we pick Z that can be written as the sum of squares three ways,
+//  so Z = B^2 + C^2 = D^2 + G^2 = E^2 + I^2
+// and then SUM = A^2 + Z.
+//
+//
+// B/C, D/G, E/I can be swapped. (There are many symmetries, so we
+// should perhaps say something like B<D? What would be the best
+// constraint?) Given those, F and H are uniquely determined, if
+// they exist.
+//
+// So basically if we have the list of Zs (and the ways of writing
+// them as sums of squares), then we loop over all A, and then
+// check the remainder of the conditions.
+
 int main(int argc, char **argv) {
   AnsiInit();
   // cl = new CL;
   // DoSearch(0x0BADBEEF, 0x0123456, 0x0777777);
   // DoSearch(29, 47, 1);
-  TestCWW();
+  // TestCWW();
+  GenCWW();
 
   return 0;
 }
