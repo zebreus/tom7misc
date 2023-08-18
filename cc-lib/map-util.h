ac713e76757208253501470b29bc4d3b1c3bbc2c
Tom7 <499244+tom7@users.noreply.github.com>
2023-08-18 01:19:31 +0000

    Add conversion from map/set to sorted vector.
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@4995 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- cc-lib/map-util.h
+++ cc-lib/map-util.h
@@ -2,6 +2,9 @@
 #ifndef _CC_LIB_MAP_UTIL_H
 #define _CC_LIB_MAP_UTIL_H
 
+#include <algorithm>
+#include <vector>
+
 // This returns a copy of the value (to avoid danging references to
 // the default) so it should generally be used when def is a small
 // value.
@@ -12,6 +15,22 @@ inline auto FindOrDefault(const M &m, const typename M::key_type &k,
   auto it = m.find(k);
   if (it == m.end()) return def;
   else return it->second;
+}
+
+template<class M>
+inline auto MapToSortedVec(const M &m) ->
+  std::vector<std::pair<typename M::key_type, typename M::mapped_type>> {
+  using K = typename M::key_type;
+  using V = typename M::mapped_type;
+  std::vector<std::pair<K, V>> vec;
+  vec.reserve(m.size());
+  for (const auto &[k, v] : m) vec.emplace_back(k, v);
+  std::sort(vec.begin(), vec.end(),
+            [](const std::pair<K, V> &a,
+               const std::pair<K, V> &b) {
+              return a.first < b.first;
+            });
+  return vec;
 }
 
 #endif
