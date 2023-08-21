a00d334a33a319c4ad921ea3e7f250d0cbfd4ab6
Tom7 <499244+tom7@users.noreply.github.com>
2023-08-21 13:53:08 +0000

    Iterate in the hopes of reducing error. It seems there are not really two choices, though?
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@5001 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- sos/recurrence.cc
+++ sos/recurrence.cc
@@ -1,6 +1,7 @@
 
 #include "bignum/big.h"
 
+#include <unordered_set>
 #include <string>
 #include <vector>
 #include <cstdint>
@@ -8,8 +9,10 @@
 #include "base/logging.h"
 #include "bignum/big-overloads.h"
 #include "ansi.h"
-
+#include "timer.h"
+#include "periodically.h"
 #include "util.h"
+#include "bhaskara-util.h"
 
 using namespace std;
 
@@ -22,12 +25,28 @@ static BigInt BigFromFile(const string &f) {
   return x;
 }
 
-static void Recur(
+static BigInt SquareError(const BigInt &aa) {
+  // PERF GMP offers a "mpz_sqrtrem"
+  BigInt a1 = BigInt::Sqrt(aa);
+  BigInt a2 = a1 + 1;
+  BigInt aa1 = a1 * a1;
+  BigInt aa2 = a2 * a2;
+  // TODO: We should consider if it's better to only
+  // approach from one side?
+  return std::min(BigInt::Abs(aa - aa1),
+                  BigInt::Abs(aa - aa2));
+}
+
+#if 0
+// TODO: search more than one bit ahead
+static std::pair<BigInt, BigInt> Recur(
     const BigInt &p,
     const BigInt &q,
     const BigInt &r,
     const BigInt &s,
-    BigInt b, BigInt c, int depth) {
+    const BigInt &b,
+    const BigInt &c,
+    int depth) {
   if (!depth) return;
 
   if (CHECK_INVARIANTS) {
@@ -43,7 +62,7 @@ static void Recur(
 
     CHECK((cc - 1_b) % 360721_b == 0_b);
     CHECK((bb - 1_b) % 222121_b == 0_b);
-    printf("divisible :)\n");
+    // printf("divisible :)\n");
 
     BigInt a1 = (cc - 1_b) / 360721_b;
     BigInt a2 = (bb - 1_b) / 222121_b;
@@ -69,6 +88,195 @@ static void Recur(
 
   Recur(p, q, r, s, b3, c3, depth - 1);
 }
+#endif
+
+struct BC {
+  BC() : BC(0, 0) {}
+  BC(BigInt bb, BigInt cc) : b(std::move(bb)),
+                             c(std::move(cc)) {}
+  BC(int64_t bb, int64_t cc) : b(bb), c(cc) {}
+  inline void Swap(BC *other) {
+    b.Swap(&other->b);
+    c.Swap(&other->c);
+  }
+
+  BC(const BC &other) : b(other.b), c(other.c) {}
+
+  BC &operator =(const BC &other) {
+    // Self-assignment does nothing.
+    if (this == &other) return *this;
+    b = other.b;
+    c = other.c;
+    return *this;
+  }
+  BC &operator =(BC &&other) {
+    // other must remain valid.
+    Swap(&other);
+    return *this;
+  }
+
+  BigInt b, c;
+};
+
+struct HashBC {
+  size_t operator()(const BC &bc) const {
+    return (size_t)(BigInt::LowWord(bc.c) * 0x314159 +
+                    BigInt::LowWord(bc.b));
+  }
+};
+
+namespace std {
+template <> struct hash<BC> {
+  size_t operator()(const BC &bc) const {
+    return HashBC()(bc);
+  }
+};
+}
+
+static inline bool operator ==(const BC &x, const BC &y) {
+  return x.c == y.c && x.b == y.b;
+}
+
+using BCSet = std::unordered_set<BC, HashBC>;
+
+#define TERM_B AFGCOLOR(232, 237, 173, "%s")
+#define TERM_C AFGCOLOR(160, 237, 237, "%s")
+
+#define TERM_PB AFGCOLOR(173, 237, 173, "%s")
+#define TERM_QC AFGCOLOR(250, 200, 173, "%s")
+#define TERM_SC AFGCOLOR(170, 130, 250, "%s")
+#define TERM_RB AFGCOLOR(120, 250, 200, "%s")
+
+static constexpr bool DEDUP = false;
+static constexpr bool VERBOSE = true;
+
+static void Greedy(
+    const BigInt &p,
+    const BigInt &q,
+    const BigInt &r,
+    const BigInt &s,
+    const BigInt &b_orig,
+    const BigInt &c_orig) {
+
+  Timer timer;
+  Periodically status_per(5.0);
+  BCSet seen;
+
+  BigInt b = b_orig;
+  BigInt c = c_orig;
+
+  static constexpr int64_t MAX_ITERS = 3;
+  for (int64_t iters = 0; MAX_ITERS < 0 || iters < MAX_ITERS; iters++) {
+
+    if (VERBOSE) {
+      printf("Iter %lld.\n"
+             "b: " TERM_B "\n"
+             "c: " TERM_C "\n",
+             iters,
+             LongNum(b).c_str(),
+             LongNum(c).c_str());
+    }
+
+    if (CHECK_INVARIANTS) {
+      if (DEDUP) {
+        CHECK(seen.find(BC(b, c)) == seen.end());
+      }
+      BigInt bb = b * b;
+      BigInt cc = c * c;
+
+      BigInt res = 360721_b * bb - 222121_b * cc;
+
+      CHECK(res == 138600_b);
+
+      // c^2 = 360721 a^2 + 1
+      // b^2 = 222121 a^2 + 1
+
+      CHECK((cc - 1_b) % 360721_b == 0_b);
+      CHECK((bb - 1_b) % 222121_b == 0_b);
+      // printf("divisible :)\n");
+
+      BigInt a1 = (cc - 1_b) / 360721_b;
+      BigInt a2 = (bb - 1_b) / 222121_b;
+
+      CHECK(a1 == a2);
+      const BigInt &aa = a1;
+
+      // const BigInt aa = a * a;
+      CHECK(360721_b * aa + 1_b == cc);
+      CHECK(222121_b * aa + 1_b == bb);
+    }
+
+    if (DEDUP) {
+      seen.insert(BC(b, c));
+    }
+
+    BigInt pb = p * b;
+    BigInt qc = q * c;
+    BigInt rb = r * b;
+    BigInt sc = s * c;
+
+    // Two options:
+    BigInt b2 = pb + qc;
+    BigInt c2 = sc + rb;
+
+    BigInt b3 = pb - qc;
+    BigInt c3 = sc - rb;
+
+    BigInt cc2 = c2 * c2;
+    // Compute the resulting a for each one.
+    // PERF: DivExact.
+    // PERF: We only need one of bb or cc!
+    BigInt a2 = (cc2 - 1) / 360721_b;
+
+    BigInt cc3 = c3 * c3;
+    BigInt a3 = (cc3 - 1) / 360721_b;
+
+    BigInt err2 = SquareError(a2);
+    BigInt err3 = SquareError(a3);
+
+    if (VERBOSE) {
+      printf("[" APURPLE("0") "] b: "
+             TERM_PB " + " TERM_QC "\n"
+             "[" APURPLE("0") "] c: "
+             TERM_SC " + " TERM_RB "\n",
+             LongNum(pb).c_str(),
+             LongNum(qc).c_str(),
+             LongNum(sc).c_str(),
+             LongNum(rb).c_str());
+    }
+
+    BigInt *err;
+    char bit;
+    if (seen.find(BC(b2, c2)) == seen.end() &&
+        err2 < err3) {
+      b = std::move(b2);
+      c = std::move(c2);
+      err = &err2;
+      // does this always lead back to the predecessor?
+      bit = '0';
+    } else if (seen.find(BC(b3, c3)) == seen.end()) {
+      b = std::move(b3);
+      c = std::move(c3);
+      err = &err3;
+      bit = '1';
+    } else {
+      printf(ARED("stuck") "\n");
+      return;
+    }
+    if (!VERBOSE) {
+      printf("%c", bit);
+    }
+
+    status_per.RunIf([&]() {
+        const int digs = (int)err->ToString().size();
+        double sec = timer.Seconds();
+        double ips = iters / sec;
+        printf("\n%d digits. %s (%.2f/sec)\n",
+               digs, ANSI::Time(sec).c_str(), ips);
+        fflush(stdout);
+      });
+  }
+}
 
 int main(int argc, char **argv) {
   ANSI::Init();
@@ -87,40 +295,10 @@ int main(int argc, char **argv) {
   BigInt r = BigFromFile("r.txt");
   BigInt s = BigFromFile("s.txt");
 
-  Recur(p, q, r, s, x2, y2, 8);
+  // Recur(p, q, r, s, x2, y2, 8);
 
-
-  #if 0
-  BigInt bb = b * b;
-  BigInt cc = c * c;
-
-  BigInt res = 360721_b * bb - 222121_b * cc;
-
-  CHECK(res == 138600_b);
-
-  // c^2 = 360721 a^2 + 1
-  // b^2 = 222121 a^2 + 1
-
-  CHECK((cc - 1_b) % 360721_b == 0_b);
-  CHECK((bb - 1_b) % 222121_b == 0_b);
-  printf("divisible :)\n");
-
-  BigInt a1 = (cc - 1_b) / 360721_b;
-  BigInt a2 = (bb - 1_b) / 222121_b;
-
-  CHECK(a1 == a2);
-  const BigInt &aa = a1;
-
-  // const BigInt aa = a * a;
-  CHECK(360721_b * aa + 1_b == cc);
-  CHECK(222121_b * aa + 1_b == bb);
-
-  const BigInt a = BigInt::Sqrt(aa);
-  CHECK(a * a == aa) << (a * a - aa).ToString();
-
-  printf("%s\n",
-         a.ToString().c_str());
-  #endif
+  // Greedy(p, q, r, s, x2, y2);
+  Greedy(p, q, r, s, x1, y1);
 
   return 0;
 }
