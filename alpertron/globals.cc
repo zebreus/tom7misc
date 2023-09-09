44f8d2ae42b9b32340ceef250ffb050d7be597a1
Tom7 <499244+tom7@users.noreply.github.com>
2023-09-09 14:51:39 +0000

    Zero the output to avoid valgrind uninitialized warning
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@5057 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- alpertron/globals.cc
+++ alpertron/globals.cc
@@ -1,9 +1,16 @@
 #include "globals.h"
 
 #include <cstdlib>
+#include <cstring>
 
 #include "bignbr.h"
 
-char *output = new char[2'000'000'000];
+static char *GetOutput() {
+  char *output = new char[2'000'000'000];
+  memset(output, 0, 2'000'000'000);
+  return output;
+}
+
+char *output = GetOutput();
 
 
