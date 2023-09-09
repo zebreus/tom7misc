93215e291de292a2aa3163eb97741a3b41d1d670
Tom7 <499244+tom7@users.noreply.github.com>
2023-09-09 19:30:31 +0000

    No longer need karatsuba or fft code, as GMP does the multiplications. It's faster, too
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@5060 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- alpertron/multiply.h
+++ alpertron/multiply.h
@@ -1,16 +1,9 @@
 #ifndef _MULTIPLY_H
 #define _MULTIPLY_H
 
-// XXX make headers conditional too
-#include "karatsuba.h"
 #include "bigmultiply.h"
 
-#if 0
-  #define multiply multiplyKaratsuba
-  #define multiplyWithBothLen multiplyWithBothLenKaratsuba
-#else
-  #define multiply BigMultiply
-  #define multiplyWithBothLen BigMultiplyWithBothLen
-#endif
+#define multiply BigMultiply
+#define multiplyWithBothLen BigMultiplyWithBothLen
 
 #endif
--- alpertron/test.cc
+++ alpertron/test.cc
@@ -7,40 +7,11 @@
 #include "bignum/big-overloads.h"
 #include "base/logging.h"
 
-#include "karatsuba.h"
-#include "bigmultiply.h"
-
-static void TestMult(const char *s1, const char *s2) {
-  BigInteger a, b;
-  CHECK(ParseBigInteger(s1, &a));
-  CHECK(ParseBigInteger(s2, &b));
-
-
-  BigInteger r;
-  r.sign = SIGN_POSITIVE;
-  multiplyWithBothLenKaratsuba(a.limbs, b.limbs, r.limbs,
-                               a.nbrLimbs, b.nbrLimbs, &r.nbrLimbs);
-
-  BigInt br = BigIntegerToBigInt(&r);
-
-  BigInt rr = BigInt(s1) * BigInt(s2);
-  CHECK(BigInt::Eq(br, rr)) << br.ToString() << " vs\n" << rr.ToString();
-}
-
+// No longer Karatsuba to test against!
+// This makes room for new tests :)
 
 int main(int argc, char **argv) {
 
-  TestMult("0", "0");
-  TestMult("1", "0");
-  TestMult("0", "1");
-  TestMult("1", "1");
-  TestMult("2", "3");
-  TestMult("2147483648", "2");
-  TestMult("2147483647", "2");
-  TestMult("2147483649", "2");
-  TestMult("2147483648", "2147483648");
-  TestMult("2983741982734981741",
-           "100000000000000000000000000000100000000000000000000");
   printf("OK\n");
   return 0;
 }
