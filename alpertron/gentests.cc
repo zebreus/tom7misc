5755fc5cb18e5ad698fb3c7447e47f69b876ffa9
Tom7 <499244+tom7@users.noreply.github.com>
2023-09-30 22:35:22 +0000

    Add a bunch of new coverage tests, generated
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@5141 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- alpertron/gentests.cc
+++ alpertron/gentests.cc
@@ -7,7 +7,7 @@
 
 int main(int argc, char **argv) {
 
-  ArcFour rc("gentests");
+  ArcFour rc("gentests2");
 
   auto RandInt = [&rc]() {
       int64_t r = (int64_t)RandTo(&rc, 65536) - 32768;
@@ -18,15 +18,15 @@ int main(int argc, char **argv) {
   for (int i = 0; i < NUM_EXAMPLES; i++) {
 
     // ax^2 + bxy + cy^2 + dx + ey = -f
-    BigInt a((rc.Byte() < 200) ? RandInt() - 32768 : 0);
-    BigInt b((rc.Byte() < 128) ? RandInt() - 32768 : 0);
-    BigInt c((rc.Byte() < 200) ? RandInt() - 32768 : 0);
-    BigInt d((rc.Byte() < 128) ? RandInt() - 32768 : 0);
-    BigInt e((rc.Byte() < 128) ? RandInt() - 32768 : 0);
+    BigInt a((rc.Byte() < 200) ? RandInt() : 0);
+    BigInt b((rc.Byte() < 128) ? RandInt() : 0);
+    BigInt c((rc.Byte() < 200) ? RandInt() : 0);
+    BigInt d((rc.Byte() < 128) ? RandInt() : 0);
+    BigInt e((rc.Byte() < 128) ? RandInt() : 0);
 
     // the solution
-    BigInt x(RandInt() - 32768);
-    BigInt y(RandInt() - 32768);
+    BigInt x(RandInt());
+    BigInt y(RandInt());
 
     // now compute f
     BigInt negf = a * x * x + b * x * y + c * y * y + d * x + e * y;
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
     BigInt f = -negf;
 
     printf("# x = %s, y = %s\n"
-           "./quad.exe %s %s %s %s %s %s 0\n",
+           "./quad.exe %s %s %s %s %s %s 0 >> coverage.out\n",
            x.ToString().c_str(),
            y.ToString().c_str(),
 
