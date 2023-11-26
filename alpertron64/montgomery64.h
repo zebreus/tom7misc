09bea4576278671d51be10376da3a212d4a1b725
Tom7 <499244+tom7@users.noreply.github.com>
2023-11-26 22:52:37 +0000

    Mark Montgomery operations as constexpr
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@5403 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- alpertron64/montgomery64.h
+++ alpertron64/montgomery64.h
@@ -3,6 +3,8 @@
 
 #include "base/int128.h"
 
+// TODO: To cc-lib. Could also support 32 and 128 bit?
+
 // A 64-bit number in montgomery form.
 struct Montgomery64 {
   // As a representation invariant, this will be in [0, modulus).
@@ -17,7 +19,7 @@ struct MontgomeryRep64 {
   // (2^64)^2 mod modulus.
   uint64_t r_squared;
 
-  MontgomeryRep64(uint64_t modulus) : modulus(modulus) {
+  explicit constexpr MontgomeryRep64(uint64_t modulus) : modulus(modulus) {
     // PERF can be done in fewer steps with tricks
     inv = 1;
     // PERF don't need this many steps!
@@ -45,13 +47,9 @@ struct MontgomeryRep64 {
     // PERF necessary?
     x %= modulus;
     return Mult(Montgomery64{.x = x}, One());
-    // x * r mod modulus
-    // return BasicModMultU64(x, r, modulus);
   }
 
-  // TODO: Everything
-
-  inline Montgomery64 Add(Montgomery64 a, Montgomery64 b) const {
+  inline constexpr Montgomery64 Add(Montgomery64 a, Montgomery64 b) const {
     // PERF: Should be able to do this without int128
     uint128_t aa(a.x);
     uint128_t bb(b.x);
@@ -62,7 +60,7 @@ struct MontgomeryRep64 {
     return Montgomery64{.x = (uint64_t)ss};
   }
 
-  inline Montgomery64 Sub(Montgomery64 a, Montgomery64 b) const {
+  inline constexpr Montgomery64 Sub(Montgomery64 a, Montgomery64 b) const {
     // PERF: Should be able to do this without int128
     int128_t aa(a.x);
     int128_t bb(b.x);
@@ -73,21 +71,22 @@ struct MontgomeryRep64 {
     return Montgomery64{.x = (uint64_t)ss};
   }
 
-  inline Montgomery64 Mult(Montgomery64 a, Montgomery64 b) const {
+  inline constexpr Montgomery64 Mult(Montgomery64 a, Montgomery64 b) const {
     uint128_t aa(a.x);
     uint128_t bb(b.x);
     uint64_t r = Reduce(aa * bb);
     return Montgomery64{.x = r};
   }
 
-  // ?
-  inline uint64_t ToInt(Montgomery64 a) const {
+  // Convert from montgomery form back to integer.
+  // Result will be in [0, modulus).
+  inline constexpr uint64_t ToInt(Montgomery64 a) const {
     return Reduce((uint128_t)a.x);
   }
 
  private:
 
-  inline uint64_t Reduce(uint128_t x) const {
+  inline constexpr uint64_t Reduce(uint128_t x) const {
     // ((x % 2^64) * inv) % 2^64
     uint64_t q = Uint128Low64(x) * inv;
     // (x / 2^64) - (q * modulus) / 2^64
@@ -98,19 +97,6 @@ struct MontgomeryRep64 {
     if (a < 0)
       a += modulus;
     return (uint64_t)a;
-  }
-
-  // Normal multiplication, not montgomery.
-  static inline uint64_t BasicModMultU64(uint64_t a, uint64_t b,
-                                         uint64_t m) {
-    // PERF: Check that this is making appropriate simplifications
-    // knowing that each high word is zero.
-    uint128 aa(a);
-    uint128 bb(b);
-    uint128 mm(m);
-
-    uint128 rr = (aa * bb) % mm;
-    return (uint64_t)rr;
   }
 };
 
