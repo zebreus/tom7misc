cce872ad994f7e4ac4291b53974c818809cc9fce
Tom7 <499244+tom7@users.noreply.github.com>
2024-09-03 23:43:59 +0000

    Add little tool to generate pages for font-image.cc
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@5985 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

---
+++ bit7/unicoderange.cc
@@ -0,0 +1,46 @@
+
+// Needs UnicodeData.txt, e.g. from
+// https://www.unicode.org/Public/14.0.0/ucd/UnicodeData.txt
+
+#include <cstdio>
+#include <cstdint>
+
+#include "util.h"
+#include "base/logging.h"
+#include "ansi.h"
+
+static void Dump(uint32_t start, uint32_t end) {
+  std::unordered_map<uint32_t, std::string> names;
+
+  for (std::string line : Util::NormalizeLines(
+           Util::ReadFileToLines("UnicodeData.txt"))) {
+    std::vector<std::string> fields = Util::Split(line, ';');
+    if (fields.size() >= 2) {
+      uint32_t codepoint = strtol(fields[0].c_str(), nullptr, 16);
+      names[codepoint] = fields[1];
+    }
+  }
+
+  // Now output.
+  for (uint32_t codepoint = start; codepoint < end; codepoint++) {
+    printf("  0x%04x,  // (%s) %s\n",
+           codepoint,
+           Util::EncodeUTF8(codepoint).c_str(),
+           names[codepoint].c_str());
+  }
+
+}
+
+
+int main(int argc, char **argv) {
+  ANSI::Init();
+  CHECK(argc == 3) << "./unicoderange.exe 2200 22FF\n";
+
+  uint32_t start = strtol(argv[1], nullptr, 16);
+  uint32_t end = strtol(argv[2], nullptr, 16);
+  CHECK(start < end && start != 0) << "./unicoderange.exe 2200 22FF\n";
+
+  Dump(start, end);
+
+  return 0;
+}
