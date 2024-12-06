3f1a338ab9916c30836c75f7b21fcd66d2caf2c1
Tom7 <499244+tom7@users.noreply.github.com>
2024-12-06 15:26:18 +0000

    gpuperts... move machines
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@6155 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- ruperts/gpuperts.cc
+++ ruperts/gpuperts.cc
@@ -1,6 +1,7 @@
 
 #include <CL/cl.h>
 #include <ctime>
+#include <string>
 #include <vector>
 
 #include "ansi.h"
@@ -10,6 +11,7 @@
 
 #include "opencl/clutil.h"
 #include "polyhedra.h"
+#include "util.h"
 
 static CL *cl = nullptr;
 
@@ -20,6 +22,26 @@ struct RupertGPU {
                          num_vertices(poly.vertices.size()),
                          width(width) {
     CHECK(width > 0);
+
+    std::string defines =
+      StringPrintf("#define NUM_VERTICES %d\n",
+                   num_vertices);
+
+    std::string kernel_src =
+      defines +
+      Util::ReadFile("gpuperts.cl");
+    const auto &[program, kernels] = cl->BuildKernels(
+        kernel_src,
+        {"RotateAndProject"},
+        1);
+
+    auto RequireKernel = [&kernels](const char *name) -> cl_kernel {
+        auto it = kernels.find(name);
+        CHECK(it != kernels.end()) << name;
+        return it->second;
+      };
+
+    rot_kernel = RequireKernel("RotateAndProject");
   }
 
   void InitializeQuats(cl_mem quats_gpu) {
@@ -43,32 +65,54 @@ struct RupertGPU {
     InitializeQuats(inner_quats);
 
     // Now, repeatedly:
-    //
+
+    for (int iter = 0; iter < 1000; iter++) {
     // Transform vertices using each quat to make the outer projection and
     // inner projection. This gives us the 2D coordinates.
-    //
-    // Apply the translation to the inner projection.
-    //
-    // Compute the loss, which is the distance from each vertex on the inner
-    // projection to the outer projection's convex hull (or zero if inside).
-    // This can also be computed as the minimum distance to any triangle
-    // in the outer projection (or zero if inside), which is more
-    // parallelizable and possibly easier to implement. (We do have to
-    // worry about consistently classifying points very close to a shared
-    // edge, however.)
-    //
-    // Do the same for a few nearby points, to estimate the gradient wrt
-    // each parameter.
-    //
-    // (Of course, if any are successful, succeed!)
-    //
-    // Perform gradient descent, updating the parameters.
+      {
+        CHECK_SUCCESS(clSetKernelArg(rot_kernel, 0, sizeof (cl_mem),
+                                     (void *)&outer_quats));
+
+        // apply to both outer and inner.
+      }
+
+      //
+      // Apply the translation to the inner projection.
+      //
+      // Compute the loss, which is the distance from each vertex on the inner
+      // projection to the outer projection's convex hull (or zero if inside).
+      // This can also be computed as the minimum distance to any triangle
+      // in the outer projection (or zero if inside), which is more
+      // parallelizable and possibly easier to implement. (We do have to
+      // worry about consistently classifying points very close to a shared
+      // edge, however.)
+      //
+      // Do the same for a few nearby points, to estimate the gradient wrt
+      // each parameter.
+      //
+      // (Of course, if any are successful, succeed!)
+      //
+      // Perform gradient descent, updating the parameters.
+
+    }
+  }
+
+  ~RupertGPU() {
+    CHECK_SUCCESS(clReleaseKernel(rot_kernel));
+    CHECK_SUCCESS(clReleaseProgram(program));
+
+    CHECK_SUCCESS(clReleaseMemObject(outer_quats));
+    CHECK_SUCCESS(clReleaseMemObject(inner_quats));
+    // XXX...
   }
 
   ArcFour rc;
   const Polyhedron &poly;
   const int num_vertices = 0;
   const int width = 0;
+
+  cl_program program;
+  cl_kernel rot_kernel;
 
   // size num_vertices * 3. The polyhedron in its original orientation.
   cl_mem vertices;
@@ -85,6 +129,7 @@ struct RupertGPU {
   // Temporary storage. Projected to 2D. Size width * num_vertices * 2.
   cl_mem outer_vertices;
   cl_mem inner_vertices;
+
 };
 
 
