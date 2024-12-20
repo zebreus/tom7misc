c230f5e38f7b7cc584ae5c2c52361fc63ee0c325
Tom7 <499244+tom7@users.noreply.github.com>
2024-12-20 23:47:14 +0000

    Scoreboard collates by 'hedron
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@6182 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- ruperts/scoreboard.cc
+++ ruperts/scoreboard.cc
@@ -1,22 +1,52 @@
 
-#include "solutions.h"
-
 #include <cstdio>
+#include <set>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "ansi.h"
+#include "solutions.h"
+
+using Attempt = SolutionDB::Attempt;
+using Solution = SolutionDB::Solution;
 
 static void PrintAll() {
-  SolutionDB db;
-  std::vector<SolutionDB::Solution> sols = db.GetAllSolutions();
-  for (const SolutionDB::Solution &sol : sols) {
-    printf(AYELLOW("%s") " via " AWHITE("%s") ":\n"
-           "  time: %lld\n"
-           "  ratio: %.17g\n",
-           sol.polyhedron.c_str(),
-           SolutionDB::MethodName(sol.method),
-           sol.createdate,
-           sol.ratio);
+
+  std::unordered_map<std::string, std::vector<Attempt>> attmap;
+  std::unordered_map<std::string, std::vector<Solution>> solmap;
+  std::set<std::string> names;
+
+  {
+    SolutionDB db;
+    std::vector<Attempt> atts = db.GetAllAttempts();
+    std::vector<Solution> sols = db.GetAllSolutions();
+
+    for (const Attempt &att : atts) {
+      names.insert(att.polyhedron);
+      attmap[att.polyhedron].push_back(att);
+    }
+
+    for (const Solution &sol : sols) {
+      names.insert(sol.polyhedron);
+      solmap[sol.polyhedron].push_back(sol);
+    }
+  }
+
+  for (const std::string &name : names) {
+    printf(AWHITE("%s") ":\n", name.c_str());
+    // const std::vector<Attempt> &atts = attmap[name];
+    for (const Attempt &att : attmap[name]) {
+      printf("  %lld iters of " ARED("%s") ", best " AORANGE("%.11g") "\n",
+             att.iters, SolutionDB::MethodName(att.method),
+             att.best_error);
+    }
+
+    for (const Solution &sol : solmap[name]) {
+      printf("  " AGREEN("%s") " solved @" APURPLE("%.11g") "\n",
+             SolutionDB::MethodName(sol.method),
+             sol.ratio);
+    }
   }
 }
 
