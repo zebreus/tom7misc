7f53b65a458601435e2f2a3be3b04dab964abfeb
Tom7 <499244+tom7@users.noreply.github.com>
2025-01-18 16:42:36 +0000

    Use rational coefficients and bignums; move most implementation to cc file. Not testing the rational stuff yet
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@6246 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- cc-lib/bignum/polynomial.cc
+++ cc-lib/bignum/polynomial.cc
@@ -1,4 +1,4 @@
-#include "polynomial.h"
+#include "bignum/polynomial.h"
 
 #include <string>
 #include <map>
@@ -8,6 +8,9 @@
 #include <utility>
 
 #include "base/logging.h"
+#include "base/stringprintf.h"
+#include "bignum/big-overloads.h"
+#include "bignum/big.h"
 
 using namespace std;
 
@@ -65,6 +68,111 @@ static std::pair<int, int> ClosePowers(std::set<int> *es,
   es->insert(ecs_other);
   return make_pair(ecs, ecs_other);
 }
+
+std::string Term::ToString() const {
+  std::string ret;
+  for (const auto &[x, e] : product) {
+    if (e == 1) ret += x;
+    else StringAppendF(&ret, "%s^%d", x.c_str(), e);
+  }
+  return ret;
+}
+
+std::pair<Term, int> Term::Divide(const Term &n, const Term &d) {
+  // we have eg. t = x^3y^2z^4 and src = xz^2. Then
+  // we to factor this to xy^2 * (xz^2)^2. xy^2 is the "remainder"
+  // and the outer ^2 on (xz^2)^2 is the power.
+
+  // Find the highest power.
+  std::optional<int> max_power = std::nullopt;
+  for (const auto &[x, e] : d.product) {
+    // XXX check if this makes sense for negative powers?
+    CHECK(e > 0);
+    auto it = n.product.find(x);
+    // If it's not present, we can exit early.
+    if (it == n.product.end()) return std::make_pair(n, 0);
+    int f = it->second;
+    CHECK(f > 0);
+    int q = f / e;
+    if (q == 0) return std::make_pair(n, 0);
+    if (!max_power.has_value() || max_power.value() > q) {
+      max_power = {q};
+    }
+  }
+
+  if (!max_power.has_value()) {
+    // This means d is 1. The result is ambiguous so we choose 0.
+    CHECK(d.product.empty()) << "bug";
+    return std::make_pair(n, 0);
+  }
+
+  int p = max_power.value();
+  CHECK(p > 0) << "bug";
+
+  // Now we know that d^q divides the term. Compute the
+  // remainder.
+  Term rem;
+  for (const auto &[x, e] : n.product) {
+    auto it = d.product.find(x);
+    if (it == d.product.end()) {
+      // Unaffected term (as though f = 0).
+      rem.product[x] = e;
+    } else {
+      int f = it->second;
+      CHECK(f * p > 0) << f << " " << p;
+      CHECK((e / (f * p)) > 0);
+      int r = e % (f * p);
+      if (r != 0) {
+        rem.product[x] = r;
+      }
+    }
+  }
+
+  return std::make_pair(rem, p);
+}
+
+
+Term Term::Times(const Term &a, const Term &b) {
+  // Same kind of thing as adding polynomials. Find common terms and
+  // add their exponents.
+  Term r;
+
+  auto ait = a.product.begin();
+  auto bit = b.product.begin();
+
+  while (ait != a.product.end() && bit != b.product.end()) {
+    if (ait->first == bit->first) {
+      // Terms share a variable. Add the exponents. They might
+      // completely cancel.
+      int new_e = ait->second + bit->second;
+      if (new_e != 0) {
+        r.product[ait->first] = ait->second + bit->second;
+      }
+      ++ait;
+      ++bit;
+    } else if (ait->first < bit->first) {
+      r.product[ait->first] = ait->second;
+      ++ait;
+    } else {
+      r.product[bit->first] = bit->second;
+      ++bit;
+    }
+  }
+
+  // Add any leftovers.
+  while (ait != a.product.end()) {
+    r.product[ait->first] = ait->second;
+    ++ait;
+  }
+
+  while (bit != b.product.end()) {
+    r.product[bit->first] = bit->second;
+    ++bit;
+  }
+
+  return r;
+}
+
 
 string Polynomial::ToCode(const string &type, const Polynomial &p) {
   // Collect all powers of variables >1 and <-1.
@@ -129,7 +237,7 @@ string Polynomial::ToCode(const string &type, const Polynomial &p) {
 
   StringAppendF(&code, "  // %d summands\n", p.sum.size());
 
-  auto TermCode = [&PowVar](int coeff, const Term &term) {
+  auto TermCode = [&PowVar](int64_t coeff, const Term &term) {
       CHECK(coeff != 0);
       // collect numerator, denominator
       std::vector<std::string> numer, denom;
@@ -166,7 +274,13 @@ string Polynomial::ToCode(const string &type, const Polynomial &p) {
   {
     int sidx = 0;
     for (const auto &[t, c] : p.sum) {
-      string tc = TermCode(c, t);
+      const auto &[a, b] = c.Parts();
+      const auto numero = a.ToInt();
+      CHECK(numero.has_value() && b == BigInt(1)) << "For ToCode, the "
+        "coefficients must be integers that fit in the radix. Got: " <<
+        c.ToString();
+
+      string tc = TermCode(numero.value(), t);
       string var = SummandVar(sidx);
       StringAppendF(&code, "  %s %s = %s;\n",
                     type.c_str(),
@@ -192,4 +306,155 @@ string Polynomial::ToCode(const string &type, const Polynomial &p) {
   StringAppendF(&code, ";\n");
 
   return code;
+}
+
+Polynomial Polynomial::Exp(const Polynomial &a, int n) {
+  CHECK(n >= 0);
+  // PERF can use repeated squaring.
+  Polynomial ret{1};
+  for (int i = 0; i < n; i++) ret = Times(ret, a);
+  return ret;
+}
+
+Polynomial Polynomial::Plus(const Polynomial &a, const Polynomial &b) {
+  Polynomial r;
+
+  auto ait = a.sum.begin();
+  auto bit = b.sum.begin();
+
+  while (ait != a.sum.end() && bit != b.sum.end()) {
+    if (ait->first == bit->first) {
+      // Polynomials share a term; add the coefficients. They could
+      // completely cancel.
+      BigRat coeff = ait->second + bit->second;
+      if (BigRat::Sign(coeff) != 0) {
+        r.sum[ait->first] = coeff;
+      }
+      ++ait;
+      ++bit;
+    } else if (ait->first < bit->first) {
+      r.sum[ait->first] = ait->second;
+      ++ait;
+    } else {
+      r.sum[bit->first] = bit->second;
+      ++bit;
+    }
+  }
+
+  // Add any leftovers.
+  while (ait != a.sum.end()) {
+    r.sum[ait->first] = ait->second;
+    ++ait;
+  }
+
+  while (bit != b.sum.end()) {
+    r.sum[bit->first] = bit->second;
+    ++bit;
+  }
+
+  return r;
+}
+
+Polynomial Polynomial::Scale(const Polynomial &p, const BigRat &s) {
+  Polynomial out;
+  if (BigRat::Sign(s) != 0) {
+    for (const auto &[t, c] : p.sum) {
+      out.sum[t] = s * c;
+    }
+  }
+  return out;
+}
+
+Polynomial Polynomial::Times(const Polynomial &a, const Polynomial &b) {
+  Polynomial r;
+
+  for (const auto &[t1, c1] : a.sum) {
+    for (const auto &[t2, c2] : b.sum) {
+      Term rt = Term::Times(t1, t2);
+
+      // PERF we could have an in-place addition of a single term.
+      r = Plus(r, Polynomial(rt, c1 * c2));
+    }
+  }
+
+  return r;
+}
+
+Polynomial Polynomial::PartialDerivative(const Polynomial &p,
+                                         const std::string &x) {
+  Polynomial r;
+  for (const auto &[t, c] : p.sum) {
+    auto it = t.product.find(x);
+    if (it == t.product.end()) {
+      // a term that doesn't depend on x; so it is dropped from
+      // the derivative
+    } else {
+      int exponent = it->second;
+      Term rt = t;
+      // reduce its exponent
+      if (exponent == 1) {
+        rt.product.erase(x);
+      } else {
+        rt.product[x] = exponent - 1;
+      }
+      // e.g. 3x^7 yields (7 * 3)x^6
+      r.sum[rt] = c * BigRat(exponent);
+    }
+  }
+
+  return r;
+}
+
+
+bool Polynomial::Eq(const Polynomial &a, const Polynomial &b) {
+  if (a.sum.size() != b.sum.size()) return false;
+  for (const auto &[t, c] : a.sum) {
+    auto it = b.sum.find(t);
+    if (it == b.sum.end()) return false;
+    if (it->second != c) return false;
+  }
+  return true;
+}
+
+Polynomial Polynomial::SubstTerm(const Term &t, const Term &src,
+                                 const Polynomial &dst) {
+  // we have eg. t = x^3y^2z^4 and src = xz^2. Then
+  // we to factor this to xy^2 * (xz^2)^2. xy^2 is the "remainder"
+  // and the outer ^2 on (xz^2)^2 is the power.
+  const auto [rem, power] = Term::Divide(t, src);
+  Polynomial out;
+  // Could just be 1.
+  out.sum = {{rem, BigInt(1)}};
+
+  return Times(out, Exp(dst, power));
+}
+
+Polynomial Polynomial::Subst(const Polynomial &p, const Term &src,
+                             const Polynomial &dst) {
+  Polynomial out;
+  for (const auto &[t, c] : p.sum) {
+    Polynomial one = SubstTerm(t, src, dst);
+    out = Plus(out, Scale(one, c));
+  }
+  return out;
+}
+
+std::string Polynomial::ToString() const {
+  if (sum.empty()) return "0";
+  // normally these would be sorted by descending power; we could
+  // do that here?
+  std::string ret;
+  for (const auto &[t, c] : sum) {
+    if (!ret.empty()) ret += " + ";
+    if (c == BigRat(1)) {
+      std::string ts = t.ToString();
+      // Term can be empty; need to explicitly represent the unit.
+      if (ts.empty()) ret += "1";
+      ret += ts;
+    } else {
+      StringAppendF(&ret, "%s%s", c.ToString().c_str(),
+                    t.ToString().c_str());
+    }
+  }
+  return ret;
 }
--- cc-lib/bignum/polynomial.h
+++ cc-lib/bignum/polynomial.h
@@ -5,10 +5,7 @@
 #include <string>
 #include <map>
 #include <utility>
-#include <optional>
 
-#include "base/logging.h"
-#include "base/stringprintf.h"
 #include "big.h"
 
 // In a polynomial like 3x^2y - 4y + 7, this represents one of the
@@ -19,119 +16,13 @@
 struct Term {
   Term() {}
   // returns "" for the unit term.
-  std::string ToString() const {
-    std::string ret;
-    for (const auto &[x, e] : product) {
-      if (e == 1) ret += x;
-      else StringAppendF(&ret, "%s^%d", x.c_str(), e);
-    }
-    return ret;
-  }
+  std::string ToString() const;
 
-  static std::pair<Term, int> Divide(const Term &n, const Term &d) {
-    // we have eg. t = x^3y^2z^4 and src = xz^2. Then
-    // we to factor this to xy^2 * (xz^2)^2. xy^2 is the "remainder"
-    // and the outer ^2 on (xz^2)^2 is the power.
-
-    // Find the highest power.
-    std::optional<int> max_power = std::nullopt;
-    for (const auto &[x, e] : d.product) {
-      // XXX check if this makes sense for negative powers?
-      CHECK(e > 0);
-      auto it = n.product.find(x);
-      // If it's not present, we can exit early.
-      if (it == n.product.end()) return std::make_pair(n, 0);
-      int f = it->second;
-      CHECK(f > 0);
-      int q = f / e;
-      if (q == 0) return std::make_pair(n, 0);
-      if (!max_power.has_value() || max_power.value() > q) {
-        max_power = {q};
-      }
-    }
-
-    if (!max_power.has_value()) {
-      // This means d is 1. The result is ambiguous so we choose 0.
-      CHECK(d.product.empty()) << "bug";
-      return std::make_pair(n, 0);
-    }
-
-    int p = max_power.value();
-    CHECK(p > 0) << "bug";
-
-    // Now we know that d^q divides the term. Compute the
-    // remainder.
-    Term rem;
-    for (const auto &[x, e] : n.product) {
-      auto it = d.product.find(x);
-      if (it == d.product.end()) {
-        // Unaffected term (as though f = 0).
-        rem.product[x] = e;
-      } else {
-        int f = it->second;
-        CHECK(f * p > 0) << f << " " << p;
-        CHECK((e / (f * p)) > 0);
-        int r = e % (f * p);
-        if (r != 0) {
-          rem.product[x] = r;
-        }
-      }
-    }
-
-    return std::make_pair(rem, p);
-  }
-
-  static Term Times(const Term &a, const Term &b) {
-    // Same kind of thing as adding polynomials. Find common terms and
-    // add their exponents.
-    Term r;
-
-    auto ait = a.product.begin();
-    auto bit = b.product.begin();
-
-    while (ait != a.product.end() && bit != b.product.end()) {
-      if (ait->first == bit->first) {
-        // Terms share a variable. Add the exponents. They might
-        // completely cancel.
-        int new_e = ait->second + bit->second;
-        if (new_e != 0) {
-          r.product[ait->first] = ait->second + bit->second;
-        }
-        ++ait;
-        ++bit;
-      } else if (ait->first < bit->first) {
-        r.product[ait->first] = ait->second;
-        ++ait;
-      } else {
-        r.product[bit->first] = bit->second;
-        ++bit;
-      }
-    }
-
-    // Add any leftovers.
-    while (ait != a.product.end()) {
-      r.product[ait->first] = ait->second;
-      ++ait;
-    }
-
-    while (bit != b.product.end()) {
-      r.product[bit->first] = bit->second;
-      ++bit;
-    }
-
-    return r;
-  }
+  static std::pair<Term, int> Divide(const Term &n, const Term &d);
+  static Term Times(const Term &a, const Term &b);
 
   std::map<std::string, int> product;
 };
-
-static inline bool operator ==(const Term &a, const Term &b) {
-  return a.product == b.product;
-}
-
-static inline bool operator <(const Term &a, const Term &b) {
-  return a.product < b.product;
-}
 
 // Represents a rational polynomial. Open domain of variables, just
 // represented as strings.
@@ -139,8 +30,13 @@ struct Polynomial {
   // zero polynomial
   Polynomial() {}
 
-  explicit Polynomial(int a) {
-    if (a != 0) {
+  // Create a 0-degree polynomial representing just the number.
+  explicit Polynomial(int a) : Polynomial(BigInt(a)) {}
+
+  explicit Polynomial(const BigInt &a) : Polynomial(BigRat(a)) {}
+
+  explicit Polynomial(const BigRat &a) {
+    if (BigRat::Sign(a) != 0) {
       // empty set of variables
       Term constant;
       sum = {{constant, a}};
@@ -152,180 +48,65 @@ struct Polynomial {
     if (exponent != 0) {
       t.product = {{x, exponent}};
     }
-    sum = {{t, 1}};
+    sum = {{t, BigRat(1)}};
   }
 
-  explicit Polynomial(const Term &t, int c) {
-    if (c != 0) {
+  // XXX swap order since we usually write this as "ct".
+  explicit Polynomial(const Term &t, const BigRat &c) {
+    if (BigRat::Sign(c) != 0) {
       sum = {{t, c}};
     }
   }
 
+  // XXX document
   static Polynomial SubstTerm(const Term &t, const Term &src,
-                              const Polynomial &dst) {
-    // we have eg. t = x^3y^2z^4 and src = xz^2. Then
-    // we to factor this to xy^2 * (xz^2)^2. xy^2 is the "remainder"
-    // and the outer ^2 on (xz^2)^2 is the power.
-    const auto [rem, power] = Term::Divide(t, src);
-    Polynomial out;
-    // Could just be 1.
-    out.sum = {{rem, 1}};
+                              const Polynomial &dst);
 
-    return Times(out, Exp(dst, power));
-  }
-
-  static Polynomial Exp(const Polynomial &a, int n) {
-    CHECK(n >= 0);
-    // PERF can use repeated squaring.
-    Polynomial ret{1};
-    for (int i = 0; i < n; i++) ret = Times(ret, a);
-    return ret;
-  }
+  static Polynomial Exp(const Polynomial &a, int n);
 
   // For a source term like "x^2" and a polynomial like "3yz",
   // replace all ocurrences of x^2 (including in higher powers like x^3)
   // with "3yz".
   static Polynomial Subst(const Polynomial &p,
-                          const Term &src, const Polynomial &dst) {
-    Polynomial out;
-    for (const auto &[t, c] : p.sum) {
-      Polynomial one = SubstTerm(t, src, dst);
-      out = Plus(out, Scale(one, c));
-    }
-    return out;
-  }
+                          const Term &src, const Polynomial &dst);
 
-  std::string ToString() const {
-    if (sum.empty()) return "0";
-    // normally these would be sorted by descending power; we could
-    // do that here?
-    std::string ret;
-    for (const auto &[t, c] : sum) {
-      if (!ret.empty()) ret += " + ";
-      if (c == 1) {
-        std::string ts = t.ToString();
-        // Term can be empty; need to explicitly represent the unit.
-        if (ts.empty()) ret += "1";
-        ret += ts;
-      } else {
-        StringAppendF(&ret, "%d%s", c, t.ToString().c_str());
-      }
-    }
-    return ret;
-  }
+  std::string ToString() const;
 
+  // The partial derivative of p with respect to the variable x.
   static Polynomial PartialDerivative(const Polynomial &p,
-                                      const std::string &x) {
-    Polynomial r;
-    for (const auto &[t, c] : p.sum) {
-      auto it = t.product.find(x);
-      if (it == t.product.end()) {
-        // a term that doesn't depend on x; so it is dropped from
-        // the derivative
-      } else {
-        int exponent = it->second;
-        Term rt = t;
-        // reduce its exponent
-        if (exponent == 1) {
-          rt.product.erase(x);
-        } else {
-          rt.product[x] = exponent - 1;
-        }
-        // e.g. 3x^7 yields (7 * 3)x^6
-        r.sum[rt] = c * exponent;
-      }
-    }
+                                      const std::string &x);
 
-    return r;
-  }
+  static Polynomial Scale(const Polynomial &p, const BigRat &scale);
+  static Polynomial Times(const Polynomial &a, const Polynomial &b);
+  static Polynomial Plus(const Polynomial &a, const Polynomial &b);
 
-  static Polynomial Scale(const Polynomial &p, int s) {
-    Polynomial out;
-    if (s != 0) {
-      for (const auto &[t, c] : p.sum) {
-        out.sum[t] = s * c;
-      }
-    }
-    return out;
-  }
-
-  static Polynomial Times(const Polynomial &a, const Polynomial &b) {
-    Polynomial r;
-
-    for (const auto &[t1, c1] : a.sum) {
-      for (const auto &[t2, c2] : b.sum) {
-        Term rt = Term::Times(t1, t2);
-
-        // PERF we could have an in-place addition of a single term.
-        r = Plus(r, Polynomial(rt, c1 * c2));
-      }
-    }
-
-    return r;
-  }
-
-  static Polynomial Plus(const Polynomial &a, const Polynomial &b) {
-    Polynomial r;
-
-    auto ait = a.sum.begin();
-    auto bit = b.sum.begin();
-
-    while (ait != a.sum.end() && bit != b.sum.end()) {
-      if (ait->first == bit->first) {
-        // Polynomials share a term; add the coefficients. They could
-        // completely cancel.
-        int coeff = ait->second + bit->second;
-        if (coeff != 0) {
-          r.sum[ait->first] = coeff;
-        }
-        ++ait;
-        ++bit;
-      } else if (ait->first < bit->first) {
-        r.sum[ait->first] = ait->second;
-        ++ait;
-      } else {
-        r.sum[bit->first] = bit->second;
-        ++bit;
-      }
-    }
-
-    // Add any leftovers.
-    while (ait != a.sum.end()) {
-      r.sum[ait->first] = ait->second;
-      ++ait;
-    }
-
-    while (bit != b.sum.end()) {
-      r.sum[bit->first] = bit->second;
-      ++bit;
-    }
-
-    return r;
-  }
-
-  static bool Eq(const Polynomial &a, const Polynomial &b) {
-    if (a.sum.size() != b.sum.size()) return false;
-    for (const auto &[t, c] : a.sum) {
-      auto it = b.sum.find(t);
-      if (it == b.sum.end()) return false;
-      if (it->second != c) return false;
-    }
-    return true;
-  }
+  static bool Eq(const Polynomial &a, const Polynomial &b);
 
   // This belongs in polynomial-util or needs to be made more
   // general.
   static std::string ToCode(const std::string &type, const Polynomial &p);
 
-  // Each term mapped to its integer coefficient. The coefficient
+  // Each term mapped to its rational coefficient. The coefficient
   // may not be zero.
-  std::map<Term, int> sum;
+  std::map<Term, BigRat> sum;
 };
+
+
+// Inline operators follow.
+
+inline bool operator ==(const Term &a, const Term &b) {
+  return a.product == b.product;
+}
+
+inline bool operator <(const Term &a, const Term &b) {
+  return a.product < b.product;
+}
+
 
 inline Polynomial operator -(const Polynomial &p) {
   Polynomial np = p;
   for (auto &tc : np.sum)
-    tc.second = -tc.second;
+    tc.second = BigRat::Negate(std::move(tc.second));
   return np;
 }
 
@@ -345,20 +126,28 @@ inline Polynomial operator *(const Polynomial &a, const Polynomial &b) {
   return Polynomial::Times(a, b);
 }
 
-inline Polynomial operator *(const Polynomial &a, int s) {
+inline Polynomial operator *(const Polynomial &a, const BigRat &s) {
   return Polynomial::Scale(a, s);
 }
 
-inline Polynomial operator *(int s, const Polynomial &a) {
+inline Polynomial operator *(const BigRat &s, const Polynomial &a) {
   return Polynomial::Scale(a, s);
+}
+
+inline Polynomial operator *(int64_t s, const Polynomial &a) {
+  return Polynomial::Scale(a, BigRat(s));
+}
+
+inline Polynomial operator *(const Polynomial &a, int64_t s) {
+  return Polynomial::Scale(a, BigRat(s));
 }
 
 inline Polynomial operator -(const Polynomial &a, const Polynomial &b) {
   return a + -b;
 }
 
-inline Polynomial operator -(const Polynomial &a, int b) {
-  return Polynomial::Plus(a, Polynomial(-b));
+inline Polynomial operator -(const Polynomial &a, const BigRat &b) {
+  return Polynomial::Plus(a, Polynomial(BigRat::Negate(b)));
 }
 
 inline bool operator ==(const Polynomial &a, const Polynomial &b) {
