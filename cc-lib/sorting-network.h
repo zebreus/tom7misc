c272f0310b3557800e3c284783b68518c544a407
Tom7 <499244+tom7@users.noreply.github.com>
2025-02-23 20:29:33 +0000

    Add sorting networks up to size 16, and tests
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@6334 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- cc-lib/sorting-network.h
+++ cc-lib/sorting-network.h
@@ -8,6 +8,10 @@
 //
 // TODO: Especially when the argument is a tuple, it is useful
 // to have a functional version that returns the sorted tuple.
+//
+// TODO: Sometimes this produces code I like (e.g. cmov), but other
+// times it just makes a huge series of branches. We should see
+// if there ways to coax the compiler.
 
 #include <array>
 #include <cstddef>
@@ -35,9 +39,320 @@ requires requires (Cont *c) {
   std::get<2>(*c);
 }
 inline void FixedSort3(Cont *c, const Cmp &cmp) {
-  CAS(0, 2);
-  CAS(0, 1);
-  CAS(1, 2);
+  CAS(0, 2); CAS(0, 1); CAS(1, 2);
+}
+
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<3>(*c);
+}
+inline void FixedSort4(Cont *c, const Cmp &cmp) {
+  CAS(0,2); CAS(1,3);
+  // --
+  CAS(0,1); CAS(2,3);
+  // --
+  CAS(1,2);
+}
+
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<4>(*c);
+}
+inline void FixedSort5(Cont *c, const Cmp &cmp) {
+  CAS(0,3); CAS(1,4);
+  // --
+  CAS(0,2); CAS(1,3);
+  // --
+  CAS(0,1); CAS(2,4);
+  // --
+  CAS(1,2); CAS(3,4);
+  // --
+  CAS(2,3);
+}
+
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<5>(*c);
+}
+inline void FixedSort6(Cont *c, const Cmp &cmp) {
+  CAS(0,5); CAS(1,3); CAS(2,4);
+  // --
+  CAS(1,2); CAS(3,4);
+  // --
+  CAS(0,3); CAS(2,5);
+  // --
+  CAS(0,1); CAS(2,3); CAS(4,5);
+  // --
+  CAS(1,2); CAS(3,4);
+}
+
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<6>(*c);
+}
+inline void FixedSort7(Cont *c, const Cmp &cmp) {
+  CAS(0,6); CAS(2,3); CAS(4,5);
+  // --
+  CAS(0,2); CAS(1,4); CAS(3,6);
+  // --
+  CAS(0,1); CAS(2,5); CAS(3,4);
+  // --
+  CAS(1,2); CAS(4,6);
+  // --
+  CAS(2,3); CAS(4,5);
+  // --
+  CAS(1,2); CAS(3,4); CAS(5,6);
+}
+
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<7>(*c);
+}
+inline void FixedSort8(Cont *c, const Cmp &cmp) {
+  CAS(0,2); CAS(1,3); CAS(4,6); CAS(5,7);
+  // --
+  CAS(0,4); CAS(1,5); CAS(2,6); CAS(3,7);
+  // --
+  CAS(0,1); CAS(2,3); CAS(4,5); CAS(6,7);
+  // --
+  CAS(2,4); CAS(3,5);
+  // --
+  CAS(1,4); CAS(3,6);
+  // --
+  CAS(1,2); CAS(3,4); CAS(5,6);
+}
+
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<8>(*c);
+}
+inline void FixedSort9(Cont *c, const Cmp &cmp) {
+  CAS(0,3); CAS(1,7); CAS(2,5); CAS(4,8);
+  // --
+  CAS(0,7); CAS(2,4); CAS(3,8); CAS(5,6);
+  // --
+  CAS(0,2); CAS(1,3); CAS(4,5); CAS(7,8);
+  // --
+  CAS(1,4); CAS(3,6); CAS(5,7);
+  // --
+  CAS(0,1); CAS(2,4); CAS(3,5); CAS(6,8);
+  // --
+  CAS(2,3); CAS(4,5); CAS(6,7);
+  // --
+  CAS(1,2); CAS(3,4); CAS(5,6);
+}
+
+// n.b. for 10, we use the one with the fewest CAS
+// (29), but there also exists one with 7 layers and
+// 31 CASes.
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<9>(*c);
+}
+inline void FixedSort10(Cont *c, const Cmp &cmp) {
+  CAS(0,8); CAS(1,9); CAS(2,7); CAS(3,5); CAS(4,6);
+  // --
+  CAS(0,2); CAS(1,4); CAS(5,8); CAS(7,9);
+  // --
+  CAS(0,3); CAS(2,4); CAS(5,7); CAS(6,9);
+  // --
+  CAS(0,1); CAS(3,6); CAS(8,9);
+  // --
+  CAS(1,5); CAS(2,3); CAS(4,8); CAS(6,7);
+  // --
+  CAS(1,2); CAS(3,5); CAS(4,6); CAS(7,8);
+  // --
+  CAS(2,3); CAS(4,5); CAS(6,7);
+  // --
+  CAS(3,4); CAS(5,6);
+}
+
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<10>(*c);
+}
+inline void FixedSort11(Cont *c, const Cmp &cmp) {
+  CAS(0,9); CAS(1,6); CAS(2,4); CAS(3,7); CAS(5,8);
+  // --
+  CAS(0,1); CAS(3,5); CAS(4,10); CAS(6,9); CAS(7,8);
+  // --
+  CAS(1,3); CAS(2,5); CAS(4,7); CAS(8,10);
+  // --
+  CAS(0,4); CAS(1,2); CAS(3,7); CAS(5,9); CAS(6,8);
+  // --
+  CAS(0,1); CAS(2,6); CAS(4,5); CAS(7,8); CAS(9,10);
+  // --
+  CAS(2,4); CAS(3,6); CAS(5,7); CAS(8,9);
+  // --
+  CAS(1,2); CAS(3,4); CAS(5,6); CAS(7,8);
+  // --
+  CAS(2,3); CAS(4,5); CAS(6,7);
+}
+
+// With 39 CAS, 9 layers.
+// There also exists one with 40 CAS, 8 layers.
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<11>(*c);
+}
+inline void FixedSort12(Cont *c, const Cmp &cmp) {
+  CAS(0,8); CAS(1,7); CAS(2,6); CAS(3,11); CAS(4,10); CAS(5,9);
+  // --
+  CAS(0,1); CAS(2,5); CAS(3,4); CAS(6,9); CAS(7,8); CAS(10,11);
+  // --
+  CAS(0,2); CAS(1,6); CAS(5,10); CAS(9,11);
+  // --
+  CAS(0,3); CAS(1,2); CAS(4,6); CAS(5,7); CAS(8,11); CAS(9,10);
+  // --
+  CAS(1,4); CAS(3,5); CAS(6,8); CAS(7,10);
+  // --
+  CAS(1,3); CAS(2,5); CAS(6,9); CAS(8,10);
+  // --
+  CAS(2,3); CAS(4,5); CAS(6,7); CAS(8,9);
+  // --
+  CAS(4,6); CAS(5,7);
+  // --
+  CAS(3,4); CAS(5,6); CAS(7,8);
+}
+
+// 45 CAS, 10 layers.
+// There also exists 46 CAS, 9 layers.
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<12>(*c);
+}
+inline void FixedSort13(Cont *c, const Cmp &cmp) {
+  CAS(0,12); CAS(1,10); CAS(2,9); CAS(3,7); CAS(5,11); CAS(6,8);
+  // --
+  CAS(1,6); CAS(2,3); CAS(4,11); CAS(7,9); CAS(8,10);
+  // --
+  CAS(0,4); CAS(1,2); CAS(3,6); CAS(7,8); CAS(9,10); CAS(11,12);
+  // --
+  CAS(4,6); CAS(5,9); CAS(8,11); CAS(10,12);
+  // --
+  CAS(0,5); CAS(3,8); CAS(4,7); CAS(6,11); CAS(9,10);
+  // --
+  CAS(0,1); CAS(2,5); CAS(6,9); CAS(7,8); CAS(10,11);
+  // --
+  CAS(1,3); CAS(2,4); CAS(5,6); CAS(9,10);
+  // --
+  CAS(1,2); CAS(3,4); CAS(5,7); CAS(6,8);
+  // --
+  CAS(2,3); CAS(4,5); CAS(6,7); CAS(8,9);
+  // --
+  CAS(3,4); CAS(5,6);
+}
+
+// 51 CAS, 10 layers.
+// Also have 52 CAS, 9 layers.
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<13>(*c);
+}
+inline void FixedSort14(Cont *c, const Cmp &cmp) {
+  CAS(0,1); CAS(2,3); CAS(4,5); CAS(6,7); CAS(8,9); CAS(10,11); CAS(12,13);
+  // --
+  CAS(0,2); CAS(1,3); CAS(4,8); CAS(5,9); CAS(10,12); CAS(11,13);
+  // --
+  CAS(0,4); CAS(1,2); CAS(3,7); CAS(5,8); CAS(6,10); CAS(9,13); CAS(11,12);
+  // --
+  CAS(0,6); CAS(1,5); CAS(3,9); CAS(4,10); CAS(7,13); CAS(8,12);
+  // --
+  CAS(2,10); CAS(3,11); CAS(4,6); CAS(7,9);
+  // --
+  CAS(1,3); CAS(2,8); CAS(5,11); CAS(6,7); CAS(10,12);
+  // --
+  CAS(1,4); CAS(2,6); CAS(3,5); CAS(7,11); CAS(8,10); CAS(9,12);
+  // --
+  CAS(2,4); CAS(3,6); CAS(5,8); CAS(7,10); CAS(9,11);
+  // --
+  CAS(3,4); CAS(5,6); CAS(7,8); CAS(9,10);
+  // --
+  CAS(6,7);
+}
+
+// 56 CAS, 10 layers.
+// Also exists: 57 CAS, 9 layers.
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<14>(*c);
+}
+inline void FixedSort15(Cont *c, const Cmp &cmp) {
+  CAS(1,2); CAS(3,10); CAS(4,14); CAS(5,8); CAS(6,13); CAS(7,12); CAS(9,11);
+  // --
+  CAS(0,14); CAS(1,5); CAS(2,8); CAS(3,7); CAS(6,9); CAS(10,12); CAS(11,13);
+  // --
+  CAS(0,7); CAS(1,6); CAS(2,9); CAS(4,10); CAS(5,11); CAS(8,13); CAS(12,14);
+  // --
+  CAS(0,6); CAS(2,4); CAS(3,5); CAS(7,11); CAS(8,10); CAS(9,12); CAS(13,14);
+  // --
+  CAS(0,3); CAS(1,2); CAS(4,7); CAS(5,9); CAS(6,8); CAS(10,11); CAS(12,13);
+  // --
+  CAS(0,1); CAS(2,3); CAS(4,6); CAS(7,9); CAS(10,12); CAS(11,13);
+  // --
+  CAS(1,2); CAS(3,5); CAS(8,10); CAS(11,12);
+  // --
+  CAS(3,4); CAS(5,6); CAS(7,8); CAS(9,10);
+  // --
+  CAS(2,3); CAS(4,5); CAS(6,7); CAS(8,9); CAS(10,11);
+  // --
+  CAS(5,6); CAS(7,8);
+}
+
+template<class Cont, class Cmp>
+requires requires (Cont *c) {
+  std::get<0>(*c);
+  // ...
+  std::get<15>(*c);
+}
+inline void FixedSort16(Cont *c, const Cmp &cmp) {
+  CAS(0,13); CAS(1,12); CAS(2,15); CAS(3,14); CAS(4,8); CAS(5,6);
+  CAS(7,11); CAS(9,10);
+  // --
+  CAS(0,5); CAS(1,7); CAS(2,9); CAS(3,4); CAS(6,13); CAS(8,14);
+  CAS(10,15); CAS(11,12);
+  // --
+  CAS(0,1); CAS(2,3); CAS(4,5); CAS(6,8); CAS(7,9); CAS(10,11);
+  CAS(12,13); CAS(14,15);
+  // --
+  CAS(0,2); CAS(1,3); CAS(4,10); CAS(5,11); CAS(6,7); CAS(8,9);
+  CAS(12,14); CAS(13,15);
+  // --
+  CAS(1,2); CAS(3,12); CAS(4,6); CAS(5,7); CAS(8,10); CAS(9,11); CAS(13,14);
+  // --
+  CAS(1,4); CAS(2,6); CAS(5,8); CAS(7,10); CAS(9,13); CAS(11,14);
+  // --
+  CAS(2,4); CAS(3,6); CAS(9,12); CAS(11,13);
+  // --
+  CAS(3,5); CAS(6,8); CAS(7,9); CAS(10,12);
+  // --
+  CAS(3,4); CAS(5,6); CAS(7,8); CAS(9,10); CAS(11,12);
+  // --
+  CAS(6,7); CAS(8,9);
 }
 
 // TODO: More sizes from here
@@ -47,14 +362,24 @@ inline void FixedSort3(Cont *c, const Cmp &cmp) {
 
 template<size_t N, class Cont, class Cmp>
 void FixedSort(Cont *c, const Cmp &cmp) {
-  if constexpr (N <= 1) {
-    return;
-  } else if constexpr (N == 2) {
-    FixedSort2(c, cmp);
-  } else if constexpr (N == 3) {
-    FixedSort3(c, cmp);
-  } else {
-    static_assert("Size not supported.");
+  if constexpr (N <= 1) { return; }
+  else if constexpr (N == 2) { FixedSort2(c, cmp); }
+  else if constexpr (N == 3) { FixedSort3(c, cmp); }
+  else if constexpr (N == 4) { FixedSort4(c, cmp); }
+  else if constexpr (N == 5) { FixedSort5(c, cmp); }
+  else if constexpr (N == 6) { FixedSort6(c, cmp); }
+  else if constexpr (N == 7) { FixedSort7(c, cmp); }
+  else if constexpr (N == 8) { FixedSort8(c, cmp); }
+  else if constexpr (N == 9) { FixedSort9(c, cmp); }
+  else if constexpr (N == 10) { FixedSort10(c, cmp); }
+  else if constexpr (N == 11) { FixedSort11(c, cmp); }
+  else if constexpr (N == 12) { FixedSort12(c, cmp); }
+  else if constexpr (N == 13) { FixedSort13(c, cmp); }
+  else if constexpr (N == 14) { FixedSort14(c, cmp); }
+  else if constexpr (N == 15) { FixedSort15(c, cmp); }
+  else if constexpr (N == 16) { FixedSort16(c, cmp); }
+  else {
+    static_assert(false, "Size not supported.");
   }
 }
 
@@ -66,3 +391,5 @@ void FixedSort(Cont *c) {
     };
   return FixedSort<N, Cont, decltype(F)>(c, F);
 }
+
+inline constexpr size_t MAX_FIXED_SORT_SUPPORTED = 16;
--- cc-lib/sorting-network_test.cc
+++ cc-lib/sorting-network_test.cc
@@ -7,6 +7,8 @@
 
 #include "ansi.h"
 #include "base/logging.h"
+#include "arcfour.h"
+#include "randutil.h"
 
 static void TestArray() {
   {
@@ -48,12 +50,55 @@ static void TestTuple() {
   }
 }
 
+template<size_t N>
+struct TestN {
+  void operator()() {
+    if constexpr (N <= 1) return;
+
+    ArcFour rc(std::format("test{}", N));
+    for (int i = 0; i < 100; i++) {
+      std::array<int, N> a;
+      for (int j = 0; j < (int)N; j++) {
+        a[j] = RandTo(&rc, N * 2);
+      }
+      std::array<int, N> a_in = a;
+      FixedSort<N>(&a);
+
+      for (int j = 1; j < (int)N; j++) {
+        if (!(a[j - 1] <= a[j])) {
+          printf("Input:\n");
+          for (int m = 0; m < (int)N; m++) {
+            printf("  %d\n", a_in[m]);
+          }
+          printf("Output:\n");
+          for (int m = 0; m < (int)N; m++) {
+            printf("  %d\n", a[m]);
+          }
+          LOG(FATAL) << "Not Sorted.\nSize " << N << " iter " << i;
+        }
+      }
+    }
+  }
+};
+
+template<std::size_t N, template<size_t> class F>
+inline void CountDown() {
+  if constexpr (N == 0) {
+    return;
+  } else {
+    TestN<N> test;
+    test();
+    CountDown<N - 1, F>();
+  }
+}
 
 int main(int argc, char **argv) {
   ANSI::Init();
 
   TestArray();
   TestTuple();
+
+  CountDown<MAX_FIXED_SORT_SUPPORTED, TestN>();
 
   printf("OK\n");
   return 0;
