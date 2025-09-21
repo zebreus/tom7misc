95bf1e5955e3efe270e29be136a15760652e57b7
Tom7 <499244+tom7@users.noreply.github.com>
2025-09-21 18:11:16 +0000

    Remove enormous, useless json parser from llama, and remote model fetching stuff
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@6724 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

---
+++ rephrase/llama/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 2789;
+char const *LLAMA_COMMIT = "84250014";
+char const *LLAMA_COMPILER = "clang version 17.0.6";
+char const *LLAMA_BUILD_TARGET = "x86_64-w64-windows-gnu";
