ebbfa9e04757125595d2f1eb03e3a4831d3caf86
Tom7 <499244+tom7@users.noreply.github.com>
2025-09-30 03:14:04 +0000

    Basic Sophie-Germain prime generation and JavaScript version
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@6736 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

---
+++ santa/sophie.cc
@@ -0,0 +1,124 @@
+
+#include <cstddef>
+#include <cstdint>
+#include <ctime>
+#include <format>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "ansi.h"
+#include "arcfour.h"
+#include "atomic-util.h"
+#include "base/print.h"
+#include "base/stringprintf.h"
+#include "bignum/big-overloads.h"
+#include "bignum/big.h"
+#include "crypt/cryptrand.h"
+#include "periodically.h"
+#include "randutil.h"
+#include "status-bar.h"
+#include "threadutil.h"
+
+DECLARE_COUNTERS(outer_test, inner_test);
+
+BigInt GenerateSophieGermainPrime(int bits, CryptRand *cr) {
+  auto Rand = [&]() { return cr->Word64(); };
+
+  Periodically status_per(1.0);
+  StatusBar status(1);
+
+  BigInt top = BigInt::LeftShift(BigInt{1}, bits);
+  BigInt msb = BigInt::LeftShift(BigInt{1}, bits - 1);
+
+  static constexpr int NUM_THREADS = 8;
+  std::vector<ArcFour> rcs;
+  for (int i = 0; i < NUM_THREADS; i++) {
+    std::string seed = std::format("{}.{}.", time(nullptr), i);
+    for (int w = 0; w < (bits / 64) + 1; w++) {
+      AppendFormat(&seed, "{:016x}", Rand());
+    }
+    rcs.emplace_back(seed);
+  }
+
+  BigInt sg =
+    ParallelAttempt([&](int64_t idx) -> std::optional<BigInt> {
+      ArcFour &rc = rcs[idx];
+      BigInt x = BigInt::RandTo([&rc]() { return Rand64(&rc); }, top);
+
+      x = BigInt::BitwiseOr(std::move(x), msb);
+
+      CHECK(x > 0);
+
+      // Start with an odd number.
+      if (x.IsEven()) {
+        x = x + 1;
+      }
+
+      for (int att = 0; att < 10; att++) {
+        outer_test++;
+        if (BigInt::IsProbablyPrime(x, 64)) {
+          BigInt y = x * 2 + 1;
+          inner_test++;
+          if (BigInt::IsProbablyPrime(y, 64)) {
+            return std::make_optional(x);
+          }
+        }
+        x += 2;
+        status_per.RunIf([&]() {
+            int64_t o = outer_test.Read();
+            int64_t i = inner_test.Read();
+            status.Status("{} outer, {} inner ({}%)",
+                          o, i,
+                          (i * 100.0) / o);
+          });
+      }
+
+      return std::nullopt;
+    }, NUM_THREADS);
+
+  return sg;
+}
+
+BigInt GetGenerator(const BigInt &sg, CryptRand *cr) {
+  auto Rand = [&]() { return cr->Word64(); };
+
+  BigInt p = sg * 2 + 1;
+  for (;;) {
+    BigInt g = BigInt::RandTo(Rand, p - 2) + 1;
+    CHECK(BigInt::Sign(g) == 1);
+
+    // Only 2 and sg are factors of p.
+    if (BigInt::PowMod(g, BigInt(2), p) == 1)
+      continue;
+
+    if (BigInt::PowMod(g, sg, p) == 1)
+      continue;
+
+    return g;
+  }
+}
+
+static void Gen(int bits) {
+  CryptRand cr;
+  BigInt sg = GenerateSophieGermainPrime(bits, &cr);
+  Print(ABLUE("Sophie-Germain prime") ": {}\n"
+        "\n"
+        APURPLE("Safe prime 2p+1") ": {}\n",
+        sg.ToString(), (sg * 2 + 1).ToString());
+
+  BigInt g = GetGenerator(sg, &cr);
+  Print("\n"
+        ACYAN("Generator") ": {}\n",
+        g.ToString());
+}
+
+
+int main(int argc, char **argv) {
+  ANSI::Init();
+
+  Gen(2048);
+
+  return 0;
+}
