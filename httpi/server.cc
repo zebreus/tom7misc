e0c95ef78cfd51eb80c924e4343562a725714e67
Tom7 <499244+tom7@users.noreply.github.com>
2025-10-24 04:27:02 +0000

    Add the epoll call and some more debugging output
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@6760 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- httpi/server.cc
+++ httpi/server.cc
@@ -17,6 +17,7 @@
 #include <sys/epoll.h>
 #include <fcntl.h>
 
+#include "hexdump.h"
 #include "ansi.h"
 #include "base/logging.h"
 #include "base/print.h"
@@ -37,6 +38,18 @@ enum ContentType : uint8_t {
   APPLICATION_DATA = 23,
 };
 
+static inline bool IsValidContentType(uint8_t c) {
+  switch (c) {
+  case CHANGE_CIPHER_SPEC:
+  case ALERT:
+  case HANDSHAKE:
+  case APPLICATION_DATA:
+    return true;
+  default:
+    return false;
+  }
+}
+
 // The "record layer".
 // In RFC 5246, this struct is "TLSPlaintext" and "TLSCiphertext"
 // (and TLSCompressed).
@@ -49,56 +62,113 @@ struct TLSRecord {
   std::vector<uint8_t> fragment;
 };
 
-static bool InternalWrite(int fd, std::vector<uint8_t> *v) {
-  CHECK(fd > 0);
-  if (v->empty()) return true;
-
-  int amount = send(fd, v->data(), v->size(), MSG_DONTWAIT);
-  if (amount == -1) {
-    if (errno == EAGAIN || errno == EWOULDBLOCK) {
-      // Connection still ok.
-      return true;
-    }
-
-    return false;
-  } else {
-    v->erase(v->begin(), v->begin() + amount);
-    return true;
+struct Connection {
+  Connection() {
+    send_buf.reserve(BUFFER_SIZE);
   }
-}
 
-struct ClientConnection {
-  ClientConnection(int client_fd,
-                   struct sockaddr_in client_addr)
-    : client_fd(client_fd), client_addr(client_addr) {
-    CHECK(client_fd != 0);
+  int FD() const { return fd; }
+
+  // With a already open file descriptor.
+  void SetFD(int fd_in) {
+    CHECK(fd_in > 0);
+    CHECK(fd == 0);
+    fd = fd_in;
 
     // All non-blocking connections.
-    int flags = fcntl(this->client_fd, F_GETFL, 0);
+    int flags = fcntl(fd, F_GETFL, 0);
     if (flags == -1 ||
-        -1 == fcntl(this->client_fd, F_SETFL, flags | O_NONBLOCK)) {
+        -1 == fcntl(fd, F_SETFL, flags | O_NONBLOCK)) {
       Shutdown("unable to set non-blocking");
       return;
     }
+  }
 
-    char client_ip_buf[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip_buf,
+  void SetAddress(struct sockaddr_in peer_addr) {
+    char ip_buf[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &peer_addr.sin_addr, ip_buf,
               INET_ADDRSTRLEN);
-    client_ip = client_ip_buf;
-    client_port = ntohs(client_addr.sin_port);
+    peer_ip = ip_buf;
+    port = ntohs(peer_addr.sin_port);
+  }
+
+  bool Connected() const { return fd != 0; }
+
+  bool Full() const {
+    return send_buf.size() >= BUFFER_SIZE;
+  }
+
+  bool HasSendData() const {
+    return !send_buf.empty();
+  }
+
+  void Write() {
+    if (!InternalWrite(fd, &send_buf)) {
+      // XXX: Might still be able to read data?
+      Shutdown("send failed");
+      return;
+    }
+  }
+
+  void Shutdown(std::string_view msg) {
+    Print(stderr,
+          AGREY("[CHILD {}]") " Shut down {}:{} ({}).\n",
+          getpid(), peer_ip, port, msg);
+    if (fd != 0) {
+      close(fd);
+      fd = 0;
+    }
+  }
+
+ protected:
+  bool InternalWrite(int fd, std::vector<uint8_t> *v) {
+    CHECK(fd > 0);
+    if (v->empty()) return true;
+
+    int amount = send(fd, v->data(), v->size(), MSG_DONTWAIT);
+    if (amount == -1) {
+      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        // Connection still ok.
+        return true;
+      }
+
+      return false;
+    } else {
+      v->erase(v->begin(), v->begin() + amount);
+      return true;
+    }
+  }
+
+  std::vector<uint8_t> send_buf;
+
+  // If zero, then the connection was closed (or hasn't been
+  // set yet).
+  int fd = 0;
+  struct sockaddr_in peer_addr;
+  std::string peer_ip;
+  int port = 0;
+};
+
+struct ClientConnection : public Connection {
+  ClientConnection(int client_fd,
+                   struct sockaddr_in client_addr) {
+    CHECK(client_fd != 0);
+
+    SetFD(client_fd);
+    SetAddress(client_addr);
 
     Print(stderr,
           AGREY("[CHILD {}]") " Start client with {}:{}\n",
-          getpid(), client_ip, client_port);
+          getpid(), peer_ip, port);
   }
 
   // Synchronous read. Assumes that client_fd is ready to read
   // at least one byte.
   void Read() {
-    CHECK(client_fd > 0);
+    CHECK(fd > 0);
     // PERF: Read directly into the incoming_partial buffer.
     uint8_t buf[1024];
-    int amount = recv(client_fd, &buf, 1024, MSG_DONTWAIT);
+    int amount = recv(fd, &buf, 1024, MSG_DONTWAIT);
     if (amount == -1) {
       if (errno == EAGAIN || errno == EWOULDBLOCK) {
         // Connection still ok.
@@ -114,22 +184,22 @@ struct ClientConnection {
     ParsePackets();
   }
 
-  void Write() {
-    if (!InternalWrite(client_fd, &send_buf)) {
-      // XXX: Might still be able to read data?
-      Shutdown("send failed");
-      return;
-    }
-  }
-
-  bool Connected() const {
-    return client_fd != 0;
-  }
-
   // If incoming_partial starts with any complete packets, move them
   // into the incoming queue.
   void ParsePackets() {
-    while (incoming_partial.size() >= 5) {
+    while (!incoming_partial.empty()) {
+      // Once we have any data, check to make sure this is TLS
+      // traffic.
+      if (!IsValidContentType(incoming_partial[0])) {
+        Shutdown("invalid tls record type");
+        return;
+      }
+
+      if (incoming_partial.size() < 5) {
+        // Don't even have length yet.
+        break;
+      }
+
       uint16_t length = incoming_partial[3];
       length <<= 8;
       length |= incoming_partial[4];
@@ -137,41 +207,38 @@ struct ClientConnection {
         Shutdown("record exceeds maximum length");
         return;
       }
-      if (incoming_partial.size() >= length + 5) {
-        TLSRecord rec;
-        // XXX check valid content type
-        rec.type = (ContentType)incoming_partial[0];
-        rec.version_major = incoming_partial[1];
-        rec.version_minor = incoming_partial[2];
-        rec.fragment.resize(length);
-        memcpy(rec.fragment.data(), incoming_partial.data() + 5, length);
-        incoming.push(std::move(rec));
-        // PERF could use ring buffer here
-        incoming_partial.erase(
-            incoming_partial.begin(), incoming_partial.begin() + length + 5);
-        // Could have more packets.
-        continue;
-      } else {
-        // Need more data.
+
+      if (incoming_partial.size() < length + 5) {
+        // Need more data to complete the record.
         break;
       }
+
+      TLSRecord rec;
+
+      Print("Packet with content type " AGREEN("{}") "\n",
+            incoming_partial[0]);
+
+      rec.type = (ContentType)incoming_partial[0];
+      rec.version_major = incoming_partial[1];
+      rec.version_minor = incoming_partial[2];
+      rec.fragment.resize(length);
+      memcpy(rec.fragment.data(), incoming_partial.data() + 5, length);
+      incoming.push(std::move(rec));
+      // PERF could use ring buffer here
+      incoming_partial.erase(
+          incoming_partial.begin(), incoming_partial.begin() + length + 5);
+      // Loop again; we could have more packets.
     }
   }
 
-  void Shutdown(std::string_view msg) {
-    Print(stderr,
-          AGREY("[CHILD {}]") " Shut down {}:{} ({}).\n",
-          getpid(), client_ip, client_port, msg);
-    if (client_fd) {
-      close(client_fd);
-      client_fd = 0;
-    }
+  std::optional<TLSRecord> GetNextRecord() {
+    if (incoming.empty()) return std::nullopt;
+    auto r = std::move(incoming.front());
+    incoming.pop();
+    return std::make_optional(std::move(r));
   }
 
-  bool Full() const {
-    return send_buf.size() >= BUFFER_SIZE;
-  }
-
+ private:
   // For outgoing data, we just write the bytes and let the
   // network stack buffer them.
   // For incoming data, we may receive partial records or
@@ -179,27 +246,11 @@ struct ClientConnection {
   std::queue<TLSRecord> incoming;
   // Prefix of a packet.
   std::vector<uint8_t> incoming_partial;
-
-  std::vector<uint8_t> send_buf;
-
-  // If zero, then the connection was closed.
-  int client_fd = 0;
-  struct sockaddr_in client_addr;
-  std::string client_ip;
-  int client_port = 0;
 };
 
-struct BackendConnection {
+struct BackendConnection : public Connection {
   BackendConnection() {
-    CHECK(false) << "unimplemented";
-  }
-
-  bool Connected() const {
-    return backend_fd != 0;
-  }
-
-  bool Full() const {
-    return send_buf.size() >= BUFFER_SIZE;
+    // CHECK(false) << "unimplemented";
   }
 
   void Write() {
@@ -210,6 +261,9 @@ struct BackendConnection {
     }
   }
 
+  void Read() {
+    // CHECK(false) << "unimplemented";
+  }
 
   void Shutdown(std::string_view msg) {
     Print(stderr,
@@ -251,6 +305,26 @@ struct PollSet {
     }
   }
 
+  std::string ColorString() const {
+    if (current_poll_set == 0) return AGREY("(empty)");
+    std::string ret;
+    if (current_poll_set & CLIENT_MASK) {
+      ret.append(ACYAN("C") ":");
+      if (current_poll_set & CLIENT_READ)
+        ret.append(AWHITE("R"));
+      if (current_poll_set & CLIENT_WRITE)
+        ret.append(AWHITE("W"));
+    }
+    if (current_poll_set & BACKEND_MASK) {
+      ret.append(ABLUE("B") ":");
+      if (current_poll_set & BACKEND_READ)
+        ret.append(AWHITE("R"));
+      if (current_poll_set & BACKEND_WRITE)
+        ret.append(AWHITE("W"));
+    }
+    return ret;
+  }
+
   void ConnectClient(int client_fd_in) {
     CHECK(client_fd == 0);
     CHECK(client_fd_in != 0);
@@ -287,7 +361,6 @@ struct PollSet {
   // If the client or backend is disconnected, its flags are
   // ignored (we assume it has been removed from the set).
   void UpdatePollSet(uint32_t s) {
-
     if (client_fd != 0) {
       if ((current_poll_set & CLIENT_MASK) != (s & CLIENT_MASK)) {
         InternalCtl(client_fd, false, s & CLIENT_READ, s & CLIENT_WRITE);
@@ -334,44 +407,108 @@ struct Session {
 
   // Run the session until termination.
   void Loop() {
+    Print(AWHITE("Session loop begins") "\n");
 
     constexpr int MAX_EVENTS = 10;
     struct epoll_event events[MAX_EVENTS];
     memset(events, 0, sizeof (epoll_event) * MAX_EVENTS);
 
     // Event loop.
-    for (;;) {
-
+    while (client.Connected() || backend.Connected()) {
       uint32_t epoll_set = 0;
-      if (client.Connected())
+
+      // Obviously only read if connected. But also don't
+      // read unless the other side of the proxied connection
+      // has space in the buffer. A typical case where this
+      // is important is when the backend has a fast connection
+      // (localhost) and is sending a large file, but the
+      // client does not. In this case we certainly don't want
+      // to buffer the entire file in RAM.
+      if (client.Connected() && !backend.Full())
         epoll_set |= PollSet::CLIENT_READ;
 
-      if (backend.Connected())
+      if (backend.Connected() && !client.Full())
         epoll_set |= PollSet::BACKEND_READ;
 
-      // Make socket set:
-      //  - read from client if still active
-      //  - read from backend if still active
-      //  - write to client if out queue not empty
-      //  - write to backend if out queue not empty
-      //  - detect socket closed for both
+      // And ask for write events if we have buffered data.
+      if (client.Connected() && client.HasSendData())
+        epoll_set |= PollSet::CLIENT_WRITE;
 
+      if (backend.Connected() && backend.HasSendData())
+        epoll_set |= PollSet::BACKEND_WRITE;
 
+      poll_set.UpdatePollSet(epoll_set);
 
-
-      // Use epoll to determine which events we can
-      // perform without blocking.
-      bool client_has_data = false;
-
-      if (client_has_data) {
-        client.Read();
+      Print("epoll set: {}\n", poll_set.ColorString());
+      int num_events = epoll_wait(poll_set.FD(), events, MAX_EVENTS, -1);
+      if (num_events == -1) {
+        if (errno == EINTR) continue;
+        perror("epoll_wait");
+        // Can't continue the session.
+        return;
       }
 
-      // Process packets.
-      if (!client.incoming.empty()) {
-        // TODO...
+      bool client_read = false, client_write = false;
+      bool backend_read = false, backend_write = false;
+      bool client_err = false, backend_err = false;
+
+      // Process epoll events.
+      for (int i = 0; i < num_events; i++) {
+        const struct epoll_event &event = events[i];
+        int event_fd = event.data.fd;
+        uint32_t flags = event.events;
+
+        if (client.Connected() && event_fd == client.FD()) {
+          if (flags & (EPOLLHUP | EPOLLERR | EPOLLRDHUP)) client_err = true;
+          if (flags & EPOLLIN) client_read = true;
+          if (flags & EPOLLOUT) client_write = true;
+        } else if (backend.Connected() && event_fd == backend.FD()) {
+          if (flags & (EPOLLHUP | EPOLLERR | EPOLLRDHUP)) backend_err = true;
+          // backend.Read() is not yet implemented.
+          // if (flags & EPOLLIN) backend_read = true;
+          if (flags & EPOLLOUT) backend_write = true;
+        }
       }
+
+      if (client_err) client.Shutdown("epoll event");
+      if (backend_err) client.Shutdown("epoll event");
+
+      // Writes first, to free up buffer space for reads.
+      if (client.Connected() && client_write) client.Write();
+      if (backend.Connected() && backend_write) backend.Write();
+
+      if (client.Connected() && client_read) client.Read();
+      if (backend.Connected() && backend_read) backend.Read();
+
+      // XXX cleaner way to do this?
+      if (!client.Connected()) poll_set.DisconnectClient();
+      if (!backend.Connected()) poll_set.DisconnectBackend();
+
+
+      ProcessPackets();
     }
+  }
+
+  // Process any packets that we've read from the client or
+  // backend.
+  void ProcessPackets() {
+    while (auto ro = client.GetNextRecord()) {
+      TLSRecord &r = ro.value();
+
+      Print(stderr, "client record: {}.{}.{} len={}\n"
+            "{}\n",
+            (uint8_t)r.type,
+            r.version_major,
+            r.version_minor,
+            r.fragment.size(),
+            HexDump::Color(r.fragment));
+
+      // XXX: Do it, depending on protocol state!
+
+    }
+
+    // XXX Process server packets too.
+
   }
 
   ~Session() {
@@ -379,7 +516,9 @@ struct Session {
 
   ClientConnection client;
 
-  // TODO: Proxied http connection ("backend")
+  // Proxied http connection ("backend")
+  // TODO: Make this connection once we know what site we're
+  // talking to.
   BackendConnection backend;
 };
 
