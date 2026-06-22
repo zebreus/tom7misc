96fab589538f62640affae16881d5154e9d2f95c
Tom7 <499244+tom7@users.noreply.github.com>
2026-06-22 17:13:54 +0000

    Coffee shop progress
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@7175 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- toward/layout.cc
+++ toward/layout.cc
@@ -1,21 +1,196 @@
 
-// Lay out components to form a circuit.
+#include "layout.h"
 
-// The circuit is a series of layers; each one computes a
-// cval (chute value) for some propositions at some x
-// locations. A chute value can be MIXED, which means that
-// it contains a '0' glyph or a '1' glyph indicating the
-// value of the proposition. It can also be ZERO, meaning
-// that it contains a '0' glyph only if the proposition
-// is false. Or it can be ONE, meaning that it contains
-// a '1' glyph only if the proposition is true. These last
-// two cases are called "separated" values; usually we
-// have a pair of outputs (ZERO, ONE) for a proposition
-// when working with separated values.
+#include <utility>
+#include <cstdint>
+#include <span>
+#include <vector>
 
-enum cval {
-  ZERO,
-  ONE,
-  MIXED,
-};
+#include "prop.h"
+#include "base/logging.h"
 
+
+int CellWidth() {
+  // TODO: Need catalog of inputs from SVG files; derive widths.
+
+  LOG(FATAL) << "Unimplemented";
+  return 0;
+}
+
+std::pair<int, int> GateSize(Gate g) {
+  switch (g) {
+  case SPACER: return {0, 0};
+  case AND0110: return {4, 1};
+  case NOT: return {1, 1};
+  case SEPARATOR: return {1, 2};
+  case WIRE: return {1, 1};
+  case XCHG00:
+  case XCHG01:
+  case XCHG10:
+  case XCHG11: return {2, 2};
+  case DUPSEP0011: return {1, 4};
+  case SINK: return {1, 0};
+  case CONST0:
+  case CONST1: return {0, 1};
+  default:
+    LOG(FATAL) << "Unimplemented gate in GateSize?";
+    return {0, 0};
+  }
+}
+
+std::pair<int, int> LayerSize(const Layer &layer) {
+  int inputs = 0, outputs = 0;
+  for (const Cell &c : layer) {
+    const auto &[i, o] = GateSize(c.gate);
+    inputs += i;
+    outputs += o;
+  }
+  return std::make_pair(inputs, outputs);
+}
+
+std::vector<Func> TransformCell(const Cell &cell,
+                                std::span<const Func> &in) {
+  const auto &[num_in, num_out] = GateSize(cell.gate);
+  CHECK(in.size() == num_in);
+  std::vector<Func> out;
+  out.reserve(num_out);
+
+  auto InputIdx = [&](int idx) {
+      if (cell.flip) {
+        return num_in - 1 - idx;
+      } else {
+        return idx;
+      }
+    };
+
+  auto OutputIdx = [&](int idx) {
+      if (cell.flip) {
+        return num_out - 1 - idx;
+      } else {
+        return idx;
+      }
+    };
+
+  switch (cell.gate) {
+  case SPACER: {
+    // Nothing.
+    break;
+  }
+
+  case AND0110: {
+    LOG(FATAL) << "Unimplemented";
+    break;
+  }
+
+  case NOT: {
+    const Func &f = in[InputIdx(0)];
+    CHECK(f.type == CType::MIXED);
+    out[OutputIdx(0)] = Func{.prop = -f.prop, .type = CType::MIXED};
+    break;
+  }
+
+  case SEPARATOR: {
+    // XXX Check order of outputs.
+    LOG(FATAL) << "Unimplemented";
+    break;
+  }
+
+  case WIRE: {
+    out[OutputIdx(0)] = in[InputIdx(0)];
+    break;
+  }
+
+  case XCHG00: {
+    const Func &fa = in[InputIdx(0)];
+    const Func &fb = in[InputIdx(1)];
+    CHECK(fa.type == CType::ZERO);
+    CHECK(fb.type == CType::ZERO);
+
+    out[OutputIdx(0)] = Func{.prop = fb.prop, .type = CType::ZERO};
+    out[OutputIdx(1)] = Func{.prop = fa.prop, .type = CType::ZERO};
+    break;
+  }
+
+  case XCHG01: {
+    const Func &fa = in[InputIdx(0)];
+    const Func &fb = in[InputIdx(1)];
+    CHECK(fa.type == CType::ZERO);
+    CHECK(fb.type == CType::ONE);
+
+    out[OutputIdx(0)] = Func{.prop = fb.prop, .type = CType::ONE};
+    out[OutputIdx(1)] = Func{.prop = fa.prop, .type = CType::ZERO};
+    break;
+  }
+
+  case XCHG10: {
+    const Func &fa = in[InputIdx(0)];
+    const Func &fb = in[InputIdx(1)];
+    CHECK(fa.type == CType::ONE);
+    CHECK(fb.type == CType::ZERO);
+
+    out[OutputIdx(0)] = Func{.prop = fb.prop, .type = CType::ZERO};
+    out[OutputIdx(1)] = Func{.prop = fa.prop, .type = CType::ONE};
+    break;
+  }
+
+  case XCHG11: {
+    const Func &fa = in[InputIdx(0)];
+    const Func &fb = in[InputIdx(1)];
+    CHECK(fa.type == CType::ONE);
+    CHECK(fb.type == CType::ONE);
+
+    out[OutputIdx(0)] = Func{.prop = fb.prop, .type = CType::ONE};
+    out[OutputIdx(1)] = Func{.prop = fa.prop, .type = CType::ONE};
+    break;
+  }
+
+  case DUPSEP0011: {
+    const Func &f = in[InputIdx(0)];
+    CHECK(f.type == CType::MIXED);
+    out[OutputIdx(0)] = Func{.prop = f.prop, .type = CType::ZERO};
+    out[OutputIdx(1)] = Func{.prop = f.prop, .type = CType::ZERO};
+    out[OutputIdx(2)] = Func{.prop = f.prop, .type = CType::ONE};
+    out[OutputIdx(3)] = Func{.prop = f.prop, .type = CType::ONE};
+    break;
+  }
+
+  case SINK: {
+    // Nothing.
+    break;
+  }
+
+  case CONST0: {
+    out[OutputIdx(0)] = {.prop = False(), .type = CType::MIXED};
+    break;
+  }
+
+  case CONST1: {
+    out[OutputIdx(0)] = {.prop = True(), .type = CType::MIXED};
+    break;
+  }
+
+  default:
+    LOG(FATAL) << "Unimplemented gate in TransformCell?";
+  }
+
+  CHECK(out.size() == num_out);
+  return out;
+}
+
+std::vector<Func> Transform(const Layer &layer,
+                            const std::vector<Func> &in) {
+  const auto &[num_inputs, num_outputs] = LayerSize(layer);
+  CHECK(num_inputs == in.size());
+  std::vector<Func> out;
+  out.reserve(num_outputs);
+
+  int in_idx = 0;
+  int out_idx = 0;
+  for (const Cell &cell : layer) {
+    // TODO
+
+  }
+
+  CHECK(out.size() == num_outputs);
+  return out;
+}
--- toward/layout.h
+++ toward/layout.h
@@ -4,6 +4,8 @@
 
 #include <vector>
 
+#include "prop.h"
+
 // Lay out components to form a circuit.
 
 // The circuit is a series of layers; each one computes a
@@ -18,7 +20,7 @@
 // have a pair of outputs (ZERO, ONE) for a proposition
 // when working with separated values.
 
-enum CType {
+enum class CType {
   ZERO,
   ONE,
   MIXED,
@@ -27,7 +29,7 @@ enum CType {
 struct CellIO {
   // Offset of the input/output pad within the cell.
   int xblock = 0;
-  CType type = MIXED;
+  CType type = CType::MIXED;
 };
 
 struct CellDef {
@@ -43,8 +45,57 @@ struct CellDef {
   std::vector<CellIO> outputs;
 };
 
-struct Layer {
-  // TODO
+enum Gate : uint8_t {
+  SPACER,
+  AND0110,
+  NOT,
+  SEPARATOR,
+  // TODO: The type of a wire is special; it propagates
+  // from its input.
+  WIRE,
+  XCHG00,
+  XCHG01,
+  XCHG10,
+  XCHG11,
+  DUPSEP0011,
+  SINK,
+  CONST0,
+  CONST1,
 };
+
+struct Cell {
+  Gate gate = Gate::SPACER;
+  // Gates like spacers and wires are parameterized.
+  int v = 0;
+  // Flip horizontally.
+  bool flip = false;
+};
+
+using Layer = std::vector<Cell>;
+struct Circuit {
+  std::vector<Layer> layers;
+};
+
+// number of inputs, outputs
+std::pair<int, int> GateSize(Gate gate);
+std::pair<int, int> LayerSize(const Layer &layer);
+
+int CellWidth(const Cell &cell);
+
+// Check that inputs are lined up with outputs, and that
+// their types match.
+void DRC(const Circuit &circuit);
+
+// All chutes are boolean functions (some proposition)
+// but could be represented different ways.
+struct Func {
+  Prop prop;
+  CType type;
+};
+
+// Compose the layer with the functions to get new ones. This ignores
+// the positions of the inputs/outputs (keeping only the order) and
+// aborts on length mismatch. Use DRC.
+std::vector<Func> Transform(const Layer &layer, const std::vector<Func> &funcs);
 
 #endif
