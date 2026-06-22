2e1462cef7afea9c3035bed1ccba0eec40132e1c
Tom7 <499244+tom7@users.noreply.github.com>
2026-06-22 20:16:07 +0000

    Tidy up the wires, add shape B
    git-svn-id: file:///home/runner/work/tom7misc-sync-action/tom7misc-sync-action/upstream/trunk@7177 c7e11af0-4c7a-11f1-9edd-117cb6f09d55

--- toward/circuit_test.cc
+++ toward/circuit_test.cc
@@ -9,118 +9,145 @@
 #include "prop.h"
 
 static void TestTransformConst() {
-  World world;
-  std::vector<bool> assignments;
-
   Layer layer = {Cell{.gate = CONST0}, Cell{.gate = CONST1}};
   std::vector<Func> in = {};
   std::vector<Func> out = Transform(layer, in);
   CHECK(out.size() == 2);
   CHECK(out[0].type == CType::MIXED);
   CHECK(out[1].type == CType::MIXED);
-  CHECK(!EvaluateProp(world, assignments, out[0].prop));
-  CHECK(EvaluateProp(world, assignments, out[1].prop));
+  CHECK(PropEq(out[0].prop, False()));
+  CHECK(PropEq(out[1].prop, True()));
 }
 
-static void TestTransformSpacerSinkWireSepNot() {
-  World world;
-  std::vector<bool> assignments;
+// Sink, wire, separator, not
+static void TestTransform1() {
+  Prop p0 = Prop{.p = Var{.id = 0}};
+  Prop p1 = Prop{.p = Var{.id = 1}};
+  Prop p2 = Prop{.p = Var{.id = 2}};
+  Prop p3 = Prop{.p = Var{.id = 3}};
 
-  Layer layer = {Cell{.gate = SPACER}, Cell{.gate = SINK}, Cell{.gate = WIRE},
-                 Cell{.gate = SEPARATOR}, Cell{.gate = NOT}};
-  std::vector<Func> in = {
-    Func{.prop = True(), .type = CType::MIXED},   // SINK
-    Func{.prop = False(), .type = CType::MIXED},  // WIRE
-    Func{.prop = True(), .type = CType::MIXED},   // SEPARATOR
-    Func{.prop = True(), .type = CType::MIXED},   // NOT
+  Layer layer = {
+    Cell{.gate = SPACER, .v = 24},
+    Cell{.gate = SINK},
+    Cell{.gate = WIRE, .v = -8},
+    Cell{.gate = SEPARATOR, .v = 15},
+    Cell{.gate = NOT}
   };
+  std::vector<Func> in = {
+    // Sink
+    Func{.prop = p0, .type = CType::MIXED},
+    // Wire
+    Func{.prop = p1, .type = CType::MIXED},
+    // Separator
+    Func{.prop = p2, .type = CType::MIXED},
+    // Not
+    Func{.prop = p3, .type = CType::MIXED},
+  };
+
   std::vector<Func> out = Transform(layer, in);
   CHECK(out.size() == 4);
   CHECK(out[0].type == CType::MIXED);
-  CHECK(!EvaluateProp(world, assignments, out[0].prop));
+  CHECK(PropEq(out[0].prop, p1));
   CHECK(out[1].type == CType::ZERO);
-  CHECK(EvaluateProp(world, assignments, out[1].prop));
+  CHECK(PropEq(out[1].prop, p2));
   CHECK(out[2].type == CType::ONE);
-  CHECK(EvaluateProp(world, assignments, out[2].prop));
+  CHECK(PropEq(out[2].prop, p2));
   CHECK(out[3].type == CType::MIXED);
-  CHECK(!EvaluateProp(world, assignments, out[3].prop));
+  CHECK(PropEq(out[3].prop, -p3));
 }
 
-static void TestTransformAndDup() {
-  World world;
-  std::vector<bool> assignments;
+static void TestTransform2() {
+  Prop pa = Prop{.p = Var{.id = 0}};
+  Prop pb = Prop{.p = Var{.id = 1}};
+  Prop pc = Prop{.p = Var{.id = 2}};
 
-  Layer layer = {Cell{.gate = AND0110}, Cell{.gate = DUPSEP0011}};
+  Layer layer = {
+    Cell{.gate = AND0110},
+    Cell{.gate = DUPSEP0011},
+  };
   std::vector<Func> in = {
-    Func{.prop = False(), .type = CType::ZERO},
-    Func{.prop = True(), .type = CType::ONE},
-    Func{.prop = True(), .type = CType::ONE},
-    Func{.prop = False(), .type = CType::ZERO},
-    Func{.prop = True(), .type = CType::MIXED},
+    Func{.prop = pa, .type = CType::ZERO},
+    Func{.prop = pa, .type = CType::ONE},
+    Func{.prop = pb, .type = CType::ONE},
+    Func{.prop = pb, .type = CType::ZERO},
+    Func{.prop = pc, .type = CType::MIXED},
   };
   std::vector<Func> out = Transform(layer, in);
   CHECK(out.size() == 5);
   CHECK(out[0].type == CType::MIXED);
-  CHECK(!EvaluateProp(world, assignments, out[0].prop));
+  CHECK(PropEq(out[0].prop, pa & pb));
 
   CHECK(out[1].type == CType::ZERO);
   CHECK(out[2].type == CType::ZERO);
   CHECK(out[3].type == CType::ONE);
   CHECK(out[4].type == CType::ONE);
   for (int i = 1; i <= 4; i++) {
-    CHECK(EvaluateProp(world, assignments, out[i].prop));
+    CHECK(PropEq(out[i].prop, pc));
   }
 }
 
 static void TestTransformXchg() {
-  World world;
-  std::vector<bool> assignments;
+  Prop p0 = Prop{.p = Var{.id = 0}};
+  Prop p1 = Prop{.p = Var{.id = 1}};
+  Prop p2 = Prop{.p = Var{.id = 2}};
+  Prop p3 = Prop{.p = Var{.id = 3}};
+  Prop p4 = Prop{.p = Var{.id = 4}};
+  Prop p5 = Prop{.p = Var{.id = 5}};
+  Prop p6 = Prop{.p = Var{.id = 6}};
+  Prop p7 = Prop{.p = Var{.id = 7}};
 
-  Layer layer = {Cell{.gate = XCHG00}, Cell{.gate = XCHG01},
-                 Cell{.gate = XCHG10}, Cell{.gate = XCHG11}};
+  Layer layer = {
+    Cell{.gate = XCHG00},
+    Cell{.gate = XCHG01},
+    Cell{.gate = XCHG10},
+    Cell{.gate = XCHG11}};
   std::vector<Func> in = {
-    Func{.prop = True(), .type = CType::ZERO},
-    Func{.prop = False(), .type = CType::ZERO},
-    Func{.prop = True(), .type = CType::ZERO},
-    Func{.prop = False(), .type = CType::ONE},
-    Func{.prop = False(), .type = CType::ONE},
-    Func{.prop = True(), .type = CType::ZERO},
-    Func{.prop = False(), .type = CType::ONE},
-    Func{.prop = True(), .type = CType::ONE},
+    Func{.prop = p0, .type = CType::ZERO},
+    Func{.prop = p1, .type = CType::ZERO},
+    Func{.prop = p2, .type = CType::ZERO},
+    Func{.prop = p3, .type = CType::ONE},
+    Func{.prop = p4, .type = CType::ONE},
+    Func{.prop = p5, .type = CType::ZERO},
+    Func{.prop = p6, .type = CType::ONE},
+    Func{.prop = p7, .type = CType::ONE},
   };
   std::vector<Func> out = Transform(layer, in);
   CHECK(out.size() == 8);
 
   CHECK(out[0].type == CType::ZERO);
-  CHECK(!EvaluateProp(world, assignments, out[0].prop));
+  CHECK(PropEq(out[0].prop, p1));
   CHECK(out[1].type == CType::ZERO);
-  CHECK(EvaluateProp(world, assignments, out[1].prop));
+  CHECK(PropEq(out[1].prop, p0));
 
   CHECK(out[2].type == CType::ONE);
-  CHECK(!EvaluateProp(world, assignments, out[2].prop));
+  CHECK(PropEq(out[2].prop, p3));
   CHECK(out[3].type == CType::ZERO);
-  CHECK(EvaluateProp(world, assignments, out[3].prop));
+  CHECK(PropEq(out[3].prop, p2));
 
   CHECK(out[4].type == CType::ZERO);
-  CHECK(EvaluateProp(world, assignments, out[4].prop));
+  CHECK(PropEq(out[4].prop, p5));
   CHECK(out[5].type == CType::ONE);
-  CHECK(!EvaluateProp(world, assignments, out[5].prop));
+  CHECK(PropEq(out[5].prop, p4));
 
   CHECK(out[6].type == CType::ONE);
-  CHECK(EvaluateProp(world, assignments, out[6].prop));
+  CHECK(PropEq(out[6].prop, p7));
   CHECK(out[7].type == CType::ONE);
-  CHECK(!EvaluateProp(world, assignments, out[7].prop));
+  CHECK(PropEq(out[7].prop, p6));
 }
 
 static void TestTransformFlip() {
   World world;
-  std::vector<bool> assignments;
 
-  Layer layer = {Cell{.gate = SEPARATOR, .flip = true},
-                 Cell{.gate = DUPSEP0011, .flip = true}};
+  Prop p0 = Prop{.p = Var{.id = 0}};
+  Prop p1 = Prop{.p = Var{.id = 1}};
+
+  Layer layer = {
+    Cell{.gate = SEPARATOR, .flip = true},
+    Cell{.gate = DUPSEP0011, .flip = true},
+  };
   std::vector<Func> in = {
-    Func{.prop = True(), .type = CType::MIXED},
-    Func{.prop = False(), .type = CType::MIXED},
+    Func{.prop = p0, .type = CType::MIXED},
+    Func{.prop = p1, .type = CType::MIXED},
   };
   std::vector<Func> out = Transform(layer, in);
   CHECK(out.size() == 6);
@@ -131,10 +158,10 @@ static void TestTransformFlip() {
   CHECK(out[4].type == CType::ZERO);
   CHECK(out[5].type == CType::ZERO);
 
-  CHECK(EvaluateProp(world, assignments, out[0].prop));
-  CHECK(EvaluateProp(world, assignments, out[1].prop));
+  CHECK(PropEq(out[0].prop, p0));
+  CHECK(PropEq(out[1].prop, p0));
   for (int i = 2; i <= 5; i++) {
-    CHECK(!EvaluateProp(world, assignments, out[i].prop));
+    CHECK(PropEq(out[i].prop, p1));
   }
 }
 
@@ -142,8 +169,8 @@ int main(int argc, char **argv) {
   ANSI::Init();
 
   TestTransformConst();
-  TestTransformSpacerSinkWireSepNot();
-  TestTransformAndDup();
+  TestTransform1();
+  TestTransform2();
   TestTransformXchg();
   TestTransformFlip();
 
